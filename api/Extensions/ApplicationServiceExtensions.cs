51070d0cda1cc90055ed66e47b6cde51af910c35
Reza Taba <mrtabaa@gmail.com>
2022-12-03 02:15:23 +0300

    api and mongoDb setup

--- api/Extensions/ApplicationServiceExtensions.cs
+++ api/Extensions/ApplicationServiceExtensions.cs
@@ -1,15 +1,20 @@
+
 namespace api.Extensions;
 
 public static class ApplicationServiceExtensions {
 
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
 
-        #region MongoDbSettings            
+        #region MongoDbSettings
+        ///// get values from this file: appsettings.Development.json /////
+        // get section
         services.Configure<MongoDbSettings>(config.GetSection(nameof(MongoDbSettings)));
 
-        services.AddSingleton<IMongoDbSettings>(sp =>
-        sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+        // get values
+        services.AddSingleton<IMongoDbSettings>(serviceProvider =>
+        serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
 
+        // get connectionString to the db
         services.AddSingleton<IMongoClient>(serviceProvider => {
             var uri = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
             return new MongoClient(uri.ConnectionString);
