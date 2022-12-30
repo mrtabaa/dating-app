a6451352190648c0808690c0c8aa23de55bb13f2
Reza Taba <mrtabaa@gmail.com>
2022-12-30 14:33:49 +0300

    replaced subscribe with pipe:map

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { map, Observable, ReplaySubject } from 'rxjs';
 import { UserLogin } from '../_models/account/user-login.model';
 import { UserRegister } from '../_models/account/user-register.model';
-import { UserProfile } from '../_models/user.model';
+import { User } from '../_models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,31 +12,42 @@ import { UserProfile } from '../_models/user.model';
 export class AccountService {
   private baseUrl = 'https://localhost:5001/account/';
 
-  private currentUserSource = new ReplaySubject<UserProfile | null>(1);
+  private currentUserSource = new ReplaySubject<User | null>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  register(userInput: UserRegister): Observable<UserRegister> {
-    return this.http.post<UserRegister>(this.baseUrl + 'register', userInput);
+  register(userInput: UserRegister): Observable<User | null> {
+    return this.http.post<User>(this.baseUrl + 'register', userInput)
+      .pipe(
+        map(user => {
+          if (user) {
+            this.currentUserSource.next(user);
+            localStorage.setItem('user', JSON.stringify(user));
+            return user;
+          }
+          return null;
+        })
+      );
   }
 
-  login(userInput: UserLogin): Observable<UserProfile | null> {
-    return this.http.post<UserProfile>(this.baseUrl + 'login', userInput).pipe(
-      map((user: UserProfile) => {
-        if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
-          return user;
-        }
-        
-        return null;
-      }));
+  login(userInput: UserLogin): Observable<User | null> {
+    return this.http.post<User>(this.baseUrl + 'login', userInput)
+      .pipe(
+        map(user => {
+          if (user) {
+            this.currentUserSource.next(user);
+            localStorage.setItem('user', JSON.stringify(user));
+            return user;
+          }
+          return null;
+        })
+      );
   }
 
   // used in app-component to set currentUserSource from the stored brwoser's localStorage key
   // now user can refresh the page or relaunch the browser without losing authentication
-  setCurrentUser(user: UserProfile): void {
+  setCurrentUser(user: User): void {
     this.currentUserSource.next(user);
   }
 
