ed37b9bef980985f9dd205ef2035b8af5f7ec610
Reza Taba <mrtabaa@gmail.com>
2023-02-27 23:14:38 +0300

    let Exception Middleware handle the null exception

--- api/Extensions/IdentityServiceExtensions.cs
+++ api/Extensions/IdentityServiceExtensions.cs
@@ -4,17 +4,17 @@ public static class IdentityServiceExtensions {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config) {
         string? tokenValue = config[ConstStringValues.TokenKey];
 
-        _ = tokenValue ?? throw new ArgumentNullException("tokenValue cannot be null", nameof(tokenValue));
-
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options => {
-                options.TokenValidationParameters = new TokenValidationParameters {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenValue)),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+        if (tokenValue != null) {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenValue)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
 
         return services;
     }
