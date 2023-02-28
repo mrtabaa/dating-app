173a0cd0b59bf122bc8a285eaf4bb18fe264235b
Reza Taba <mrtabaa@gmail.com>
2023-02-28 22:07:08 +0300

    added Error Handling and grouped them

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { NotFoundComponent } from './components/errors/not-found/not-found.component';
+import { NoAccessComponent } from './components/errors/no-access/no-access.component';
+import { TestErrorComponent } from './components/errors/test-error/test-error.component';
+import { ServerErrorComponent } from './components/errors/server-error/server-error.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/account/login/login.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegisterComponent } from './components/account/register/register.component';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
@@ -10,7 +15,6 @@ import { MessagesComponent } from './components/messages/messages.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthLoggedInGuard } from './guards/auth-logged-in.guard';
-import { TestErrorComponent } from './components/test-error/test-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -34,7 +38,14 @@ const routes: Routes = [
       { path: 'register', component: RegisterComponent },
     ]
   },
-  { path: 'errors', component: TestErrorComponent },
+  {
+    path: '',
+    children: [
+      { path: 'errors', component: TestErrorComponent },
+      { path: 'server-error', component: ServerErrorComponent },
+      { path: 'no-access', component: NoAccessComponent }
+    ]
+  },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' }
 ];
 
