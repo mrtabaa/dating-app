79612a17764c1620932151fee7d84e3d510f7110
Reza Taba <mrtabaa@gmail.com>
2023-05-31 05:21:07 +0300

    Validations Improvements Fixed Email validation Cleaned unused validations

--- api/DTOs/AccountDto.cs
+++ api/DTOs/AccountDto.cs
@@ -4,24 +4,27 @@ public record UserRegisterDto(
     string? Schema,
     [MinLength(2), MaxLength(20)] string Name,
     [
-        MaxLength(50),
+        MaxLength(50), 
         RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$", ErrorMessage ="Bad Email Format.")
     ] string Email,
     [DataType(DataType.Password), MinLength(7), MaxLength(20)] string Password,
     DateOnly DateOfBirth,
     string KnownAs,
     string Gender,
-    string Introduction,
-    string LookingFor,
-    string Interests,
-    string City,
-    string Country,
+    [MinLength(10), MaxLength(500)] string Introduction,
+    [MinLength(10), MaxLength(500)] string LookingFor,
+    [MinLength(10), MaxLength(500)] string Interests,
+    [MinLength(3), MaxLength(30)] string City,
+    [MinLength(3), MaxLength(30)] string Country,
     List<Photo> Photos
 );
 
 public record LoginDto(
     string? Schema,
-    [EmailAddress, MaxLength(50)] string Email,
+    [
+        RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$", ErrorMessage = "Bad Email Format."), 
+        MaxLength(50)
+    ] string Email,
     [MinLength(7), MaxLength(20)] string Password
 );
 
@@ -29,5 +32,5 @@ public record LoginSuccessDto(
     string Schema,
     string? Token,
     string? Name,
-    [EmailAddress] string? Email
+    string? Email
 );
--- api/DTOs/MemberDto.cs
+++ api/DTOs/MemberDto.cs
@@ -1,19 +1,17 @@
-namespace api.DTOs;
-
 public record MemberDto(
     string Schema,
     string Id,
-    [MinLength(2), MaxLength(20)] string Name,
-    [EmailAddress] string Email,
-    [Range(16, 99)] int Age,
-    [MinLength(3), MaxLength(20)] string KnownAs,
+    string Name,
+    string Email,
+    int Age,
+    string KnownAs,
     DateTime Created,
     DateTime LastActive,
     string Gender,
-    [MinLength(10), MaxLength(500)] string Introduction,
-    [MinLength(10), MaxLength(500)] string LookingFor,
-    [MinLength(10), MaxLength(500)] string Interests,
-    [MinLength(3), MaxLength(20)] string City,
-    [MinLength(3), MaxLength(20)] string Country,
+    string Introduction,
+    string LookingFor,
+    string Interests,
+    string City,
+    string Country,
     List<Photo> Photos
 );
