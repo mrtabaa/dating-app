3f2f35f466613bb1570666c9e6a37603ac0bbe3e
Reza Taba <mrtabaa@gmail.com>
2023-07-09 16:31:42 -0700

    improved code by moving isMain to DOM

--- client/src/app/components/members/member-card/member-card.component.ts
+++ client/src/app/components/members/member-card/member-card.component.ts
@@ -6,21 +6,8 @@ import { Member } from 'src/app/models/member.model';
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.scss']
 })
-export class MemberCardComponent implements OnInit {
+export class MemberCardComponent {
   @Input('memberInput') member: Member | undefined;
 
   mainUrl: string | undefined;
-
-  ngOnInit(): void {
-    this.setMainUrl();
-  }
-  
-  setMainUrl(): void {
-    if (this.member)
-    for (const photo of this.member.photos) {
-      if (photo.isMain) {
-        this.mainUrl = photo.url_128;
-      }
-    }
-  }
 }
