a51f2fb5f0b312e247b5fea6c50a216569e5c814
Reza Taba <mrtabaa@gmail.com>
2023-07-24 07:07:55 +0300

    more setMainPhoto

--- client/src/app/components/members/photo-editor/photo-editor.component.ts
+++ client/src/app/components/members/photo-editor/photo-editor.component.ts
@@ -1,43 +1,22 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member.model';
 import { MemberService } from 'src/app/services/member.service';
 import { environment } from 'src/environments/environment';
-
-interface PhotoUrl {
-  url_128: string,
-  isMain: boolean
-}
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
   styleUrls: ['./photo-editor.component.scss']
 })
-export class PhotoEditorComponent implements OnInit {
-  @Input('member') member$: Observable<Member> | undefined;
-  photoUrls: PhotoUrl[] = [];
+export class PhotoEditorComponent {
+  @Input('member') member: Member | undefined;
 
-  constructor(private memberService: MemberService, private cd: ChangeDetectorRef) { }
+  basePhotoUrl: string = environment.apiPhotoUrl;
 
-  ngOnInit(): void {
-    this.getPhotoUrls();
-  }
+  constructor(private memberService: MemberService) { }
 
-  getPhotoUrls(): void {
-    this.member$?.pipe(
-      map((member: Member) => {
-        for (const photo of member.photos) {
-          this.photoUrls.push(
-            {
-              url_128: environment.apiPhotoUrl + photo.url_128,
-              isMain: photo.isMain
-            });
-        }
-      }))
-  }
-
-  setMainPhoto(idIn: string | undefined, url_128In: string): void {
-    this.memberService.setMainPhoto(idIn, url_128In);
+  setMainPhoto(url_128In: string): void {
+    this.member = this.memberService.setMainPhoto(url_128In);
   }
 }
--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -2,9 +2,11 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member.model';
-import { Observable, finalize, map, of } from 'rxjs';
+import { Observable, finalize, map, of, take } from 'rxjs';
 import { MemberUpdate } from '../models/member-update.model';
 import { UpdateResult } from '../models/helpers/update-result.model';
+import { AccountService } from './account.service';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,13 @@ export class MemberService {
   baseUrl: string = environment.apiUrl + 'user';
   members: Member[] = [];
   member: Member | undefined;
+  user!: User;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => { if (user) this.user = user; }
+    });
+  }
 
   getMembers(): Observable<Member[]> {
     if (this.members.length > 0) return of(this.members); // return ObservableOf(members)
@@ -49,25 +56,29 @@ export class MemberService {
     );
   }
 
-  setMainPhoto(idIn: string | undefined, url_128In: string): Observable<UpdateResult> {
+  setMainPhoto(url_128In: string): Member | undefined {
     // cut domain address and convert the address to storage/photos/...
-    const dbFormatUrl_128 = url_128In.split('5001/')[1];
-    let queryParams = new HttpParams().set('photoUrlIn', dbFormatUrl_128);
+    let queryParams = new HttpParams().set('photoUrlIn', url_128In);
 
-    return this.http.put<UpdateResult>(this.baseUrl + '/set-main-photo', null, { params: queryParams })
-      .pipe(finalize(() => {
-        const member = this.members.find(member => member.id === idIn);
-        if (member) {
+    this.http.put<UpdateResult>(this.baseUrl + '/set-main-photo', null, { params: queryParams })
+      .pipe(take(1)).subscribe(() => {
+        const member = this.members.find(member => member.id === this.user?.id);
+        if (member && this.user) {
           member.photos.forEach(photo => {
-            if (photo.isMain === true) photo.isMain = false;
-            if (photo.url_128 === dbFormatUrl_128) photo.isMain = true;
+            if (photo.isMain === true)
+              photo.isMain = false;
+
+            if (photo.url_128 === url_128In) {
+              photo.isMain = true;
+              this.user.profilePhotoUrl = url_128In;
+              this.accountService.setCurrentUser(this.user);
+            }
           })
 
-          const index = this.members.indexOf(member);
-          this.members[index] = { ...this.members[index], ...member }
-          
           this.member = member;
         }
-      }));
+      });
+
+    return this.member;
   }
 }
