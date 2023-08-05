c41fe05826377f1ad3d5b543dc476e71d2aad0c4
Reza Taba <mrtabaa@gmail.com>
2023-08-05 11:50:55 -0700

    remove unneccessary nullables

--- api/DTOs/UserDto.cs
+++ api/DTOs/UserDto.cs
@@ -2,9 +2,9 @@ namespace api.DTOs;
 
 public record UserDto(
     string Schema,
-    string? Id,
-    string? Token,
-    string? KnownAs,
-    string? Email,
+    string Id,
+    string Token,
+    string KnownAs,
+    string Email,
     string? ProfilePhotoUrl
 );
