5214f51a36a4b871dfc3ae9d59d354e768baa384
Reza Taba <mrtabaa@gmail.com>
2023-08-07 05:24:53 +0300

    increased string length limit

--- api/DTOs/AccountDto.cs
+++ api/DTOs/AccountDto.cs
@@ -10,9 +10,9 @@ public record UserRegisterDto(
     [MinLength(2), MaxLength(30)] string KnownAs,
     DateOnly DateOfBirth,
     string Gender,
-    [MinLength(10), MaxLength(300)] string? Introduction,
-    [MinLength(10), MaxLength(300)] string? LookingFor,
-    [MinLength(10), MaxLength(300)] string? Interests,
+    [MinLength(10), MaxLength(500)] string? Introduction,
+    [MinLength(10), MaxLength(500)] string? LookingFor,
+    [MinLength(10), MaxLength(500)] string? Interests,
     [MinLength(3), MaxLength(30)] string City,
     [MinLength(3), MaxLength(30)] string Country,
     List<Photo>? Photos
