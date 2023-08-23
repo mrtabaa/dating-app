4c1f50fdd89f1185c814291c07271e47a3e3fe3c
Reza Taba <mrtabaa@gmail.com>
2023-08-23 11:47:42 -0700

    hide logged in user(currentUser) from Members list

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member.model';
+import { User } from 'src/app/models/user.model';
+import { AccountService } from 'src/app/services/account.service';
 import { MemberService } from 'src/app/services/member.service';
 
 @Component({
@@ -10,8 +12,11 @@ import { MemberService } from 'src/app/services/member.service';
 })
 export class MemberListComponent implements OnInit {
   members$: Observable<Member[]> | undefined;
+  currentUser$: Observable<User | null> | undefined;
 
-  constructor(private memberService: MemberService) { }
+  constructor(private memberService: MemberService, accountService: AccountService) {
+    this.currentUser$ = accountService.currentUser$;
+  }
 
   ngOnInit(): void {
     this.loadMembers();
