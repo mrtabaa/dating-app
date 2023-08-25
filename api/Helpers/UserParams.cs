538201226f7f022065fe0a0a7e086c67ece628ac
Reza Taba <mrtabaa@gmail.com>
2023-08-25 12:04:57 -0700

    changed prop set to init

--- api/Helpers/UserParams.cs
+++ api/Helpers/UserParams.cs
@@ -4,13 +4,12 @@ public class UserParams
 {
     private const int MaxPageSize = 50;
 
-    public int PageNumber { get; set; } = 1;
+    public int PageNumber { get; init; } = 1;
 
     private int _pageSize = 10;
     public int PageSize
     {
         get { return _pageSize; }
-        set { _pageSize =  (value > MaxPageSize) ? MaxPageSize : value; }
+        set { _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
     }
-    
 }
