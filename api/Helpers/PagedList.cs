1a286db6dddb6f20360828b084ec9327dfddd905
Reza Taba <mrtabaa@gmail.com>
2023-11-02 09:47:51 -0700

    removed unused Constructor & better naming

--- api/Helpers/PagedList.cs
+++ api/Helpers/PagedList.cs
@@ -2,19 +2,15 @@ namespace api.Helpers;
 
 public class PagedList<T> : List<T>
 {
-    public PagedList() // allows creating an empty object without constructor's arguments
-    {
-    }
-
     // set props values
-    public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+    private PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
     {
         CurrentPage = pageNumber;
-        TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
         PageSize = pageSize;
         TotalCount = count;
         AddRange(items);
-    } 
+    }
 
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
@@ -29,7 +25,7 @@ public class PagedList<T> : List<T>
     /// <param name="pageSize"></param>
     /// <param name="cancellationToken"></param>
     /// <returns>PageList<T> object with its prop values</returns>
-    public static async Task<PagedList<T>> CreateAsync(IMongoQueryable<T>? source, int pageNumber, int pageSize, CancellationToken cancellationToken)
+    public static async Task<PagedList<T>> CreatePagedListAsync(IMongoQueryable<T>? source, int pageNumber, int pageSize, CancellationToken cancellationToken)
     {
         var count = await source.CountAsync();
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
