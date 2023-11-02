885247a02b686e15368081a1bb52861ca7b46c02
Reza Taba <mrtabaa@gmail.com>
2023-11-02 09:52:24 -0700

    changed api ports to 7000 range

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api/',
-  apiPhotoUrl: 'https://localhost:5001/'
+  apiUrl: 'https://localhost:7001/api/',
+  apiPhotoUrl: 'https://localhost:7001/'
 };
 
 /*
