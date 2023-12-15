386073f54bb6138ec2c53a03120d696295985513
Reza Taba <mrtabaa@gmail.com>
2023-12-15 11:25:34 -0800

    rewrote ng2-file-upload

--- client/src/app/components/user/photo-editor/photo-editor.component.ts
+++ client/src/app/components/user/photo-editor/photo-editor.component.ts
@@ -4,14 +4,14 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { FileUploadModule, FileUploader } from 'ng2-file-upload';
 import { take } from 'rxjs';
-import { LoggedInUser } from '../../../models/loggedInUser.model';
-import { User } from '../../../models/user.model';
+import { Member } from '../../../models/member.model';
 import { environment } from '../../../../environments/environment';
 import { AccountService } from '../../../services/account.service';
 import { UserService } from '../../../services/user.service';
 import { UpdateResult } from '../../../models/helpers/update-result.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-photo-editor',
@@ -25,11 +25,11 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./photo-editor.component.scss']
 })
 export class PhotoEditorComponent implements OnInit {
-  @Input('user') user: User | undefined;
+  @Input('member') member: Member | undefined;
   private accountService = inject(AccountService);
   private userService = inject(UserService);
 
-  loggedInUser: LoggedInUser | undefined;
+  user: User | undefined;
   errorGlob: string | undefined;
   basePhotoUrl: string = environment.apiPhotoUrl;
 
@@ -38,9 +38,9 @@ export class PhotoEditorComponent implements OnInit {
 
   constructor() {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: (loggedInUser: LoggedInUser | null) => {
-        if (loggedInUser)
-          this.loggedInUser = loggedInUser
+      next: (user: User | null) => {
+        if (user)
+          this.user = user
       }
     });
   }
@@ -56,7 +56,7 @@ export class PhotoEditorComponent implements OnInit {
   initializeUploader(): void {
     this.uploader = new FileUploader({
       url: environment.apiUrl + 'user/add-photos',
-      authToken: 'Bearer ' + this.loggedInUser?.token,
+      authToken: 'Bearer ' + this.user?.token,
       isHTML5: true,
       allowedFileType: ['image'],
       removeAfterUpload: true,
@@ -71,7 +71,7 @@ export class PhotoEditorComponent implements OnInit {
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       if (response) {
         const photo = JSON.parse(response);
-        this.user?.photos.push(photo);
+        this.member?.photos.push(photo);
       }
     }
   }
@@ -79,19 +79,19 @@ export class PhotoEditorComponent implements OnInit {
   setMainPhoto(url_128In: string): void {
     this.userService.setMainPhoto(url_128In).pipe(take(1)).subscribe({
       next: (updateResult: UpdateResult) => {
-        if (updateResult.modifiedCount === 1 && this.loggedInUser && this.user) {
-          this.user.photos.forEach(photo => {
+        if (updateResult.modifiedCount === 1 && this.user && this.member) {
+          this.member.photos.forEach(photo => {
             // unset previous main
             if (photo.isMain === true)
               photo.isMain = false;
 
             // set new selected main
-            if (this.loggedInUser && photo.url_128 === url_128In) {
+            if (this.user && photo.url_128 === url_128In) {
               photo.isMain = true;
 
               // update navbar photos
-              this.loggedInUser.profilePhotoUrl = url_128In;
-              this.accountService.setCurrentUser(this.loggedInUser);
+              this.user.profilePhotoUrl = url_128In;
+              this.accountService.setCurrentUser(this.user);
             }
           })
         }
@@ -105,8 +105,8 @@ export class PhotoEditorComponent implements OnInit {
   deletePhoto(url_128In: string, index: number): void {
     this.userService.deletePhoto(url_128In).pipe(take(1)).subscribe({
       next: (updateResult: UpdateResult) => {
-        if (updateResult.modifiedCount === 1 && this.user) {
-          this.user.photos.splice(index, 1);
+        if (updateResult.modifiedCount === 1 && this.member) {
+          this.member.photos.splice(index, 1);
         }
       }
     })
