4d5f7028c9b0b9fb400451e33c3599060dadfbd3
Reza Taba <mrtabaa@gmail.com>
2023-12-16 00:19:58 -0800

    add MatButtonModule

--- client/src/app/components/errors/not-found/not-found.component.ts
+++ client/src/app/components/errors/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
