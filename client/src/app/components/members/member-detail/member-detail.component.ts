10c9245f95f3c009e9c0cb7f1b030ab643f555b2
Reza Taba <mrtabaa@gmail.com>
2024-01-04 09:05:12 -0800

    Setup gallery

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -9,13 +9,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MemberService } from '../../../services/member.service';
 import { MatButtonModule } from '@angular/material/button';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
   imports: [
     CommonModule, NgOptimizedImage,
-    MatCardModule, MatTabsModule, MatButtonModule
+    MatCardModule, MatTabsModule, MatButtonModule,
+    GalleryModule
   ],
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.scss']
@@ -27,9 +29,10 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
   member$: Observable<Member> | undefined;
   apiPhotoUrl = environment.apiPhotoUrl;
   subscribed: Subscription | undefined;
+  images: GalleryItem[] = [];
 
   ngOnInit(): void {
-    this.loadMember();
+    this.getMember();
     this.setGalleryImages();
   }
 
@@ -37,7 +40,7 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
     this.subscribed;
   }
 
-  loadMember(): void {
+  getMember(): void {
     const id: string | null = this.route.snapshot.paramMap.get('id');
 
     if (id) {
@@ -47,7 +50,10 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
 
   setGalleryImages(): void {
     this.subscribed = this.member$?.subscribe(
-      (user: Member) => {
+      (member: Member) => {
+        for (const photo of member.photos) {
+          this.images.push(new ImageItem({src: this.apiPhotoUrl + photo.url_1024, thumb: this.apiPhotoUrl + photo.url_128}));
+        }
       }
     );
   }
