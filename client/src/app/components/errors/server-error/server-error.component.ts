0c881a28a143ba7cf10908c0c72f180832c0c4c3
Reza Taba <mrtabaa@gmail.com>
2024-01-07 04:31:54 -0800

    imported MatButtonModule

--- client/src/app/components/errors/server-error/server-error.component.ts
+++ client/src/app/components/errors/server-error/server-error.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './server-error.component.html',
   styleUrls: ['./server-error.component.scss']
 })
