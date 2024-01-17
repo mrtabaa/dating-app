3808e63723cdbb561b0c64a0197c097c1d78c8bd
Reza Taba <mrtabaa@gmail.com>
2024-01-17 03:00:16 -0800

    completed filter: gender, minAge, maxAge with styling

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -10,13 +10,15 @@ import { MemberParams } from '../../../models/helpers/member-params';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../../services/account.service';
+import { MatButtonModule } from '@angular/material/button';
+import { range } from 'lodash';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
   imports: [
     MemberCardComponent, FormsModule,
-    MatPaginatorModule, MatSelectModule
+    MatPaginatorModule, MatSelectModule, MatButtonModule
   ],
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.scss']
@@ -28,24 +30,24 @@ export class MemberListComponent implements OnDestroy {
 
   subscribed: Subscription | undefined;
 
-  members: Member[] = [];
-
   pagination: Pagination | undefined;
+  members: Member[] | undefined;
   memberParams: MemberParams | undefined;
   gender: string | undefined;
-  maxAge: number | undefined;
-  minAge: number | undefined;
+  minAge: number = 18;
+  maxAge: number = 99;
+
+  // installed lodash library
+  ages: number[] = [...range(18, 100)];
 
   // Material Pagination attrs
   // pageSize = 5;
   // pageIndex = 0; // add 1 before sending to API since endpoint's pageNumber starts from 1
   pageSizeOptions = [5, 10, 25];
-
   hidePageSize = false;
   showPageSizeOptions = true;
   showFirstLastButtons = true;
   disabled = false;
-
   pageEvent: PageEvent | undefined;
 
   //#endregion
@@ -54,6 +56,7 @@ export class MemberListComponent implements OnDestroy {
   constructor() {
     effect(() => {
       const loggedInGender = this.accountService.loggedInUserSig()?.gender;
+
       if (loggedInGender)
         this.memberParams = new MemberParams(loggedInGender);
 
@@ -67,14 +70,16 @@ export class MemberListComponent implements OnDestroy {
   //#endregion auto-run methods
 
   loadFilteredMembers(): void {
+    if (!this.gender)
+      this.gender = this.memberParams?.gender;
+
     if (this.memberParams && this.gender) {
-      console.log(this.gender);
       this.memberParams = {
         pageNumber: this.memberParams.pageNumber,
         pageSize: this.memberParams.pageSize,
         gender: this.gender,
-        minAge: this.memberParams.minAge,
-        maxAge: this.memberParams.maxAge
+        minAge: this.minAge,
+        maxAge: this.maxAge
       }
     }
 
