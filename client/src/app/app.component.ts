87a24124355cc8883b8e6e21d9cbdd587a13ecc6
Reza Taba <mrtabaa@gmail.com>
2024-01-24 01:45:30 -0800

    Added footer and keep it at the bottom of the screen if not reached

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,13 +7,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserService } from './services/user.service';
 import { take } from 'rxjs';
 import { LoggedInUser } from './models/logged-in-user.model';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, RouterOutlet, NgxSpinnerModule,
-    NavbarComponent
+    NavbarComponent, FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
