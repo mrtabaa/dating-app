96edc15e3a30fc6b843e04c662cc01daff5e7057
Reza Taba <mrtabaa@gmail.com>
2024-02-02 13:40:13 -0800

    Fixed MongoDb sessions/transaction by making server as replica with one member(primary). Add 2 secondary members for production. See _resources

--- backend/api/Repositories/LikesRepository.cs
+++ backend/api/Repositories/LikesRepository.cs
@@ -53,24 +53,26 @@ public class LikesRepository : ILikesRepository
             {
                 //// Session is NOT supported in MongoDb Standalone servers!
                 // Create a session object that is used when leveraging transactions
-                // using var session = await _client.StartSessionAsync(null, cancellationToken);
+                using var session = await _client.StartSessionAsync(null, cancellationToken);
 
                 // Begin transaction
-                // session.StartTransaction();
+                session.StartTransaction();
 
                 try
                 {
                     // TODO add session to this part so if UpdateLikedByCount failed, undo the InsertOnce.
-                    await _collection.InsertOneAsync(like, null, cancellationToken);
+                    await _collection.InsertOneAsync(session, like, null, cancellationToken);
 
-                    await UpdateLikedByCount(targetMemberAppUser.Id, cancellationToken);
+                    await UpdateLikedByCount(session, targetMemberAppUser.Id, cancellationToken);
+
+                    await session.CommitTransactionAsync(cancellationToken);
 
                     likeStatus.IsSuccess = true;
                     return likeStatus; // success
                 }
                 catch (System.Exception ex)
                 {
-                    // await session.AbortTransactionAsync(cancellationToken);
+                    await session.AbortTransactionAsync(cancellationToken);
 
                     _logger.LogError("Like failed. Error writing to MongoDB" + ex.Message);
 
@@ -117,12 +119,12 @@ public class LikesRepository : ILikesRepository
     /// <param name="targetMemberId"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    private async Task UpdateLikedByCount( string? targetMemberId, CancellationToken cancellationToken)
+    private async Task UpdateLikedByCount(IClientSessionHandle session, string? targetMemberId, CancellationToken cancellationToken)
     {
         UpdateDefinition<AppUser> updateLikedByCount = Builders<AppUser>.Update
         .Inc(appUser => appUser.Liked_byCount, 1); // Increament by 1 for each like
 
-        await _collectionUsers.UpdateOneAsync<AppUser>(appUser =>
+        await _collectionUsers.UpdateOneAsync<AppUser>(session, appUser =>
                 appUser.Id == targetMemberId, updateLikedByCount, null, cancellationToken);
     }
 
