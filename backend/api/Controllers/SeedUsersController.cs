4d5ff25e2df4c8c35068223b81c44500b40daa8b
Reza Taba <mrtabaa@gmail.com>
2024-02-21 01:41:13 -0800

    Updated SeedData values and fixed controller

--- backend/api/Controllers/SeedUsersController.cs
+++ backend/api/Controllers/SeedUsersController.cs
@@ -23,9 +23,18 @@ public class SeedUsersController : BaseApiController
         // https://stackoverflow.com/a/53803908/3944285
         var command = "{ dbStats: 1, scale: 1 }";
         var dbStats = await _database.RunCommandAsync<BsonDocument>(command);
-        var databaseExists = dbStats["collections"].AsInt32 > 0 || dbStats["indexes"].AsInt32 > 0;
+        bool databaseExists;
 
-        Console.WriteLine(databaseExists);
+        if (dbStats["collections"].BsonType == BsonType.Int64)
+        {
+            var collectionsCount = dbStats["collections"].AsInt64;
+            databaseExists = collectionsCount > 0 || dbStats["indexes"].AsInt64 > 0;
+        }
+        else
+        {
+            var collectionsCount = dbStats["collections"].AsInt32;
+            databaseExists = collectionsCount > 0 || dbStats["indexes"].AsInt32 > 0;
+        }
 
         if (databaseExists == true)
             return BadRequest("Database already exists");
@@ -33,10 +42,6 @@ public class SeedUsersController : BaseApiController
         #endregion
 
         #region Import db seed
-
-        // manually dispose HMACSHA512 after being done
-        using var hmac = new HMACSHA512();
-
         // add each user to DB
         foreach (var userInput in inputUsersDummy)
         {
