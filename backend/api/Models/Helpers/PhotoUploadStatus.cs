021715ea4e49545151bca1f47b3f8ed3de38d1d6
Reza Taba <mrtabaa@gmail.com>
2024-02-21 09:15:31 -0800

    Increased MaxPhotosLimit to 100

--- backend/api/Models/Helpers/PhotoUploadStatus.cs
+++ backend/api/Models/Helpers/PhotoUploadStatus.cs
@@ -2,7 +2,7 @@ namespace api.Models.Helpers;
 
 public class PhotoUploadStatus
 {
-    public readonly int MaxPhotosLimit = 3;
+    public readonly int MaxPhotosLimit = 100;
     public bool IsMaxPhotoReached { get; set; }
     public Photo? Photo { get; set; }
 }
