d8550ac9f7322e944d5432e6852ccdce27b018d2
Reza Taba <mrtabaa@gmail.com>
2024-02-27 20:22:27 +0300

    client: Implemented edit roles with mat-select. Member's roles are pre-selected.

--- client/src/app/components/admin/member-management/member-management.component.ts
+++ client/src/app/components/admin/member-management/member-management.component.ts
@@ -1,33 +1,49 @@
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { MemberWithRole } from '../../../models/member-with-role.model';
 import { AdminService } from '../../../services/admin.service';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-member-management',
   standalone: true,
   imports: [
-    CommonModule,
-    MatTableModule, MatIconModule, MatButtonModule
+    CommonModule, FormsModule, ReactiveFormsModule,
+    MatTableModule, MatFormFieldModule, MatSelectModule, MatButtonModule
   ],
   templateUrl: './member-management.component.html',
   styleUrl: './member-management.component.scss'
 })
 export class MemberManagementComponent {
   private adminService = inject(AdminService);
+  private fb = inject(FormBuilder);
 
-  displayedColumns = ['no', 'username', 'active-roles', 'action']
+  displayedColumns = ['no', 'username', 'active-roles', 'action'];
+  displayedRoles = ['admin', 'moderator', 'member'];
+
   members$: Observable<MemberWithRole[]> | undefined;
+
+  selectedArray = this.fb.array([]);
 
   ngOnInit(): void {
     this.getMembersWithRoles();
   }
 
   getMembersWithRoles(): void {
-    this.members$ = this.adminService.getMembersWithRoles();
+    this.members$ = this.adminService.getMembersWithRoles()
+      .pipe(tap(members => members
+        .forEach(member => {
+          member.roles
+          this.selectedArray.push(this.fb.control(member.roles));
+        })));
+  }
+
+  updateRoles(i: number): void {
+    console.log(this.selectedArray.at(i).value);
   }
 }
