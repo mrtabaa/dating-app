db479479dd676169189c557b8202b50f75aa2aec
Reza Taba <mrtabaa@gmail.com>
2024-02-27 02:15:31 -0800

    Implemented getMemberWithRole() and editMemberRole()

---
+++ client/src/app/models/member-with-role.model.ts
@@ -0,0 +1,4 @@
+export interface MemberWithRole {
+    userName: string;
+    roles: string[];
+}
---
+++ client/src/app/services/admin.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { MemberWithRole } from '../models/member-with-role.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private http = inject(HttpClient);
+
+  private apiUrl: string = environment.apiUrl + 'admin/';
+
+  getMembersWithRoles(): Observable<MemberWithRole[]> {
+    return this.http.get<MemberWithRole[]>(this.apiUrl + 'users-with-roles')
+  }
+
+  editMemberRole(userName: string, selectedRoles: string[]): Observable<MemberWithRole> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('newRoles', JSON.stringify(selectedRoles))
+
+    return this.http.put<MemberWithRole>(this.apiUrl + '/' + userName, null, {params: httpParams})
+  }
+}
