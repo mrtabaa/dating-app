d71ddaa200ed2a0c809230d86972651244eb31b4
Reza Taba <mrtabaa@gmail.com>
2024-03-24 05:45:58 -0700

    Added UserSecrets + Change port on production only.

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -2,22 +2,25 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // AUTO-GENERATED CODES //
 builder.Services.AddControllers();
 
 // From customized ServiceExtensions (Extensions folder) for a claen maintained code //
-builder.Services.AddApplicationServices(builder.Configuration);
+builder.Configuration.AddUserSecrets<Program>(); // Register User Secrets
+
+builder.Services.AddApplicationServices(builder.Configuration, builder.Environment);
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddRepositoryServices();
 #endregion
 
 #region Production
-builder.Configuration.AddUserSecrets<Program>(); // Register User Secrets
 
 // Production: Set the URLs the app will listen on
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenLocalhost(5100); // Listen for incoming HTTP connections on port 5100
+    if (builder.Environment.IsProduction())
+        serverOptions.ListenLocalhost(7100); // Listen for incoming HTTP connections on port 7100
 });
 #endregion
 
