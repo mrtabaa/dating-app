efc2819c58cf715595be2361a24833c430a85563
Reza Taba <mrtabaa@gmail.com>
2024-03-30 05:36:09 -0700

    removed the test

--- client/src/app/directives/clear-control-field-by-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClearControlFieldByClick } from './clear-control-field-by-click.directive';
-
-describe('dirClearControlField', () => {
-  it('should create an instance', () => {
-    const directive = new ClearControlFieldByClick();
-    expect(directive).toBeTruthy();
-  });
-});
