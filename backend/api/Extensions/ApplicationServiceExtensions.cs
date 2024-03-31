b876fc66649666d3829e29aa8f2b50369740e4c5
Reza Taba <mrtabaa@gmail.com>
2024-03-31 09:55:23 -0700

    Added https:// to requests and CORS

--- backend/api/Extensions/ApplicationServiceExtensions.cs
+++ backend/api/Extensions/ApplicationServiceExtensions.cs
@@ -42,7 +42,7 @@ public static class ApplicationServiceExtensions
                 options.AddDefaultPolicy(policy => policy
                 .AllowAnyHeader()
                 .AllowAnyMethod()
-                .WithOrigins("da-client-mr.azurewebsites.net")); // production
+                .WithOrigins("https://da-client-mr.azurewebsites.net")); // production
             }
         });
 
