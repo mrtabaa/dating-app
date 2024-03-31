34996609fd3c6a5f9191cce9fce84867dab5ee99
Reza Taba <mrtabaa@gmail.com>
2024-03-31 03:39:02 -0700

    Fixed api url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://da-api-mr.azurewebsites.net/',
+  apiUrl: 'https://da-api-mr.azurewebsites.net/api/',
   apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/'
 };
