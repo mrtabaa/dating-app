67a6100eecb81dfbec7511ef341fa384c2288e58
Reza Taba <mrtabaa@gmail.com>
2024-04-02 09:41:58 -0700

    forced port 8080

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -21,10 +21,10 @@ builder.Services.AddRepositoryServices();
 //         serverOptions.ListenLocalhost(7100); // Listen for incoming HTTP connections on port 7100
 // });
 
-var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
+// var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenAnyIP(int.Parse(port));
+    serverOptions.ListenAnyIP(8080);
 });
 
 #region Configure the HTTP request pipeline.
