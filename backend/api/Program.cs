8ca9abee33bfd3de0076226399f136c3cff981c5
Reza Taba <mrtabaa@gmail.com>
2024-04-05 08:58:36 -0700

    Cleanup

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -3,11 +3,6 @@ using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 #region Setup Configurations
-// Register KeyVault for Azure
-// var keyVault = new Uri(builder.Configuration.GetSection("KeyVaultUrl").Value!);
-// var azureCredentials = new DefaultAzureCredential();
-// builder.Configuration.AddAzureKeyVault(keyVault, azureCredentials);
-
 // Register User Secrets for Nginx
 builder.Configuration.AddUserSecrets<Program>();
 #endregion
@@ -21,13 +16,16 @@ builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddRepositoryServices();
 #endregion
 
+#region Nginx
 // Production: Set the URLs the app will listen on
 // builder.WebHost.ConfigureKestrel(serverOptions =>
 // {
 //     if (builder.Environment.IsProduction())
 //         serverOptions.ListenLocalhost(7100); // Listen for incoming HTTP connections on port 7100
 // });
+#endregion
 
+// Production Azure: If no WEBSITE_PORT exists set to 8080
 var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
