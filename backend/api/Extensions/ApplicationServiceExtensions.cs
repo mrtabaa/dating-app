239bacb4cf2367181caa177afa5cc51772f57b28
Reza Taba <mrtabaa@gmail.com>
2024-04-05 11:54:24 -0700

    AllowAnyOrigin() with Dev

--- backend/api/Extensions/ApplicationServiceExtensions.cs
+++ backend/api/Extensions/ApplicationServiceExtensions.cs
@@ -29,22 +29,23 @@ public static class ApplicationServiceExtensions
         #region Others
         services.AddCors(options =>
         {
-            // if (env.IsDevelopment())
-            // {
-            //     options.AddDefaultPolicy(policy => policy
-            //         .AllowAnyHeader()
-            //         .AllowAnyMethod()
-            //         .WithOrigins("http://localhost:4300")
-            //     );
-            // }
-            // else if (env.IsProduction())
-            // {
+            if (env.IsDevelopment())
+            {
+                options.AddDefaultPolicy(policy => policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("http://localhost:4300")
+                );
+            }
+            else if (env.IsProduction())
+            {
             options.AddDefaultPolicy(policy => policy
                 .AllowAnyHeader()
                 .AllowAnyMethod()
-                .WithOrigins("https://da-api-mr.azurewebsites.net/api") // production
+                .AllowAnyOrigin()
+                // .WithOrigins("https://da-api-mr.azurewebsites.net/api") // production
             );
-            // }
+            }
         });
 
         services.AddScoped<LogUserActivity>(); // monitor/log userActivity
