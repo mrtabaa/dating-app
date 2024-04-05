8823604b6a57d881c4e8f4e7c1cefda8a203c2ca
Reza Taba <mrtabaa@gmail.com>
2024-04-05 11:42:55 -0700

    Changed else to else if (env.IsProduction())

--- backend/api/Extensions/ApplicationServiceExtensions.cs
+++ backend/api/Extensions/ApplicationServiceExtensions.cs
@@ -29,22 +29,22 @@ public static class ApplicationServiceExtensions
         #region Others
         services.AddCors(options =>
         {
-            // if (env.IsDevelopment())
-            // {
-            //     options.AddDefaultPolicy(policy => policy
-            //         .AllowAnyHeader()
-            //         .AllowAnyMethod()
-            //         .WithOrigins("http://localhost:4300")
-            //     );
-            // }
-            // else
-            // {
-            options.AddDefaultPolicy(policy => policy
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowAnyOrigin() // production
-            );
-            // }
+            if (env.IsDevelopment())
+            {
+                options.AddDefaultPolicy(policy => policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("http://localhost:4300")
+                );
+            }
+            else if (env.IsProduction())
+            {
+                options.AddDefaultPolicy(policy => policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("https://da-api-mr.azurewebsites.net/api") // production
+                );
+            }
         });
 
         services.AddScoped<LogUserActivity>(); // monitor/log userActivity
