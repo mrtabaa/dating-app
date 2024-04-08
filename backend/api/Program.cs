18ab6c0aed231c842a9a7c41efd301f1766dc2ab
Reza Taba <mrtabaa@gmail.com>
2024-04-08 12:20:32 +0300

    Move "WEBSITE_PORT" ?? "8080" to IsProduction

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -23,11 +23,14 @@ builder.Services.AddRepositoryServices();
 #endregion
 
 // Production Azure: If no WEBSITE_PORT exists set to 8080
-var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
-builder.WebHost.ConfigureKestrel(serverOptions =>
+if (builder.Environment.IsProduction())
 {
-    serverOptions.ListenAnyIP(int.Parse(port));
-});
+    var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(int.Parse(port));
+    });
+}
 
 #region Configure the HTTP request pipeline.
 var app = builder.Build();
