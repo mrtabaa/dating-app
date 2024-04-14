61f779f3037ac87d737c4e55e4c62c6f7befe8a5
Reza Taba <mrtabaa@gmail.com>
2024-04-14 16:46:13 +0200

    Change port to 8080 on Production only

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -22,12 +22,15 @@ builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddRepositoryServices();
 #endregion
 
-// Production Azure: If no WEBSITE_PORT exists set to 8080
-var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
-builder.WebHost.ConfigureKestrel(serverOptions =>
+if (builder.Environment.IsProduction())
 {
-    serverOptions.ListenAnyIP(int.Parse(port));
-});
+    // Production Azure: If no WEBSITE_PORT exists set to 8080
+    var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(int.Parse(port));
+    });
+}
 
 #region Configure the HTTP request pipeline.
 var app = builder.Build();
