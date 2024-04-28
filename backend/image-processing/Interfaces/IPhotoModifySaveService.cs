f89c78c640f3533b6d45a61a6a5278e6d6be261b
Reza Taba <mrtabaa@gmail.com>
2024-04-28 10:58:13 +0200

    Added CancellationToken to all methods

--- backend/image-processing/Interfaces/IPhotoModifySaveService.cs
+++ backend/image-processing/Interfaces/IPhotoModifySaveService.cs
@@ -2,11 +2,11 @@ namespace image_processing.Interfaces;
 
 public interface IPhotoModifySaveService
 {
-    public Task<string> ResizeImageByScale(IFormFile formFile, string userId, int standardSizeIndex);
-    public Task<string> ResizeByPixel(IFormFile formFile, string userId, int widthIn, int heightIn);
-    public Task<string> ResizeByPixel_Square(IFormFile formFile, string userId, int side);
-    public Task<string> Crop(IFormFile formFile, string userId, int widthIn, int heightIn);
-    public Task<string> Crop_Square(IFormFile formFile, string userId, int side);
-    public Task<string> CropWithOriginalSide_Square(IFormFile formFile, string userId);
-    public Task<string> SaveImageAsIs(IFormFile formFile, string userId, int operation);
+    public Task<string> ResizeImageByScale(IFormFile formFile, string userId, int standardSizeIndex, CancellationToken cancellationToken);
+    public Task<string> ResizeByPixel(IFormFile formFile, string userId, int widthIn, int heightIn, CancellationToken cancellationToken);
+    public Task<string> ResizeByPixel_Square(IFormFile formFile, string userId, int side, CancellationToken cancellationToken);
+    public Task<string> Crop(IFormFile formFile, string userId, int widthIn, int heightIn, CancellationToken cancellationToken);
+    public Task<string> Crop_Square(IFormFile formFile, string userId, int side, CancellationToken cancellationToken);
+    public Task<string> CropWithOriginalSide_Square(IFormFile formFile, string userId, CancellationToken cancellationToken);
+    public Task<string> SaveImageAsIs(IFormFile formFile, string userId, int operation, CancellationToken cancellationToken);
 }
