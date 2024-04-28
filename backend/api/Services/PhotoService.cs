75a65ae4e2f76be8a4ddfc20e170bbd7abe9d042
Reza Taba <mrtabaa@gmail.com>
2024-04-28 13:43:36 +0200

    Improved codes and comments

--- backend/api/Services/PhotoService.cs
+++ backend/api/Services/PhotoService.cs
@@ -7,23 +7,23 @@ public class PhotoService(IPhotoModifySaveService _photoModifyService, ILogger<I
     #endregion
 
     /// <summary>
-    /// ADD PHOTO TO DISK
-    /// Store photos on disk by creating a folder based on fileName, userId, size, crop, etc. Each user will have a folder named after their db _Id.
+    /// ADD PHOTO TO BLOB
+    /// Store photos on blob by creating a folder based on fileName, userId, size, crop, etc. Each user will have a folder named after their db _Id.
     /// Resize and square image with 165px(navbar & thumbnail), 256px(card).
     /// Scale image to a ~300kb max size for the enlarged gallery photo.
-    /// Store photo address in db as "storage/photos/user-id/resize-pixel-square/128x128/my-photo.jpg"
+    /// Store photo address in db as "user-id/resize-pixel-square/128x128/GUID_my-photo.jpg"
     /// 
     /// DELETE PHOTO FROM DISK
     /// </summary>
     /// <param name="formFile"></param>
     /// <param name="userId"></param>
-    /// <returns>ADD: array of filePaths. DELETE: boolean</returns>
+    /// <returns>Array of filePaths</returns>
     public async Task<string[]?> AddPhotoToBlob(IFormFile formFile, string? userId, CancellationToken cancellationToken)
     {
         // copy file/s to the folder
         if (formFile.Length > 0 && !string.IsNullOrEmpty(userId))
         {
-            #region Resize and/or Store Images to Disk
+            #region Resize and/or Store Images to Blob
             string? filePath_165_sq; // navbar & thumbnail
             string? filePath_256_sq; // card
             string? filePath_enlarged; // enlarged photo up to ~300kb
@@ -32,16 +32,15 @@ public class PhotoService(IPhotoModifySaveService _photoModifyService, ILogger<I
             filePath_256_sq = await _photoModifyService.ResizeByPixel_Square(formFile, userId, 256, cancellationToken); // card
             filePath_enlarged = await _photoModifyService.ResizeImageByScale(formFile, userId, (int)DimensionsEnum._4_3_800x600, cancellationToken); // enlarged photo
 
-            // if conversion fails
+            // if image processing fails
             if (filePath_165_sq is null || filePath_256_sq is null || filePath_enlarged is null)
             {
                 _logger.LogError("Photo addition failed. The returned filePath is null which is not allowed.");
                 return null;
             }
-            #endregion Resize and Create Images to Disk
+            #endregion Resize and Create Images to Blob
 
-            #region Create the photo URLs and return the result
-            // // generate "storage/photos/user-id/resize-pixel-square/128x128/my-photo.jpg"
+            #region return the result if success
             return [
                 filePath_165_sq,
                 filePath_256_sq,
