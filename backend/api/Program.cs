e8ffd1c484dc5f4db34a7cca098bba2f865b8bc5
Reza Taba <mrtabaa@gmail.com>
2024-04-29 11:01:17 +0300

    Removed Azure settings for Nginx

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -3,14 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 #region Nginx
 // Production
 
-// Register User Secrets for Nginx
-// builder.Configuration.AddUserSecrets<Program>();
+// Register User Secrets securely
+builder.Configuration.AddUserSecrets<Program>();
 
-// builder.WebHost.ConfigureKestrel(serverOptions =>
-// {
-//     if (builder.Environment.IsProduction())
-//         serverOptions.ListenLocalhost(7100); // Listen for incoming HTTP connections on port 7100
-// });
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    if (builder.Environment.IsProduction())
+        serverOptions.ListenLocalhost(7100); // Make Nginx listen for incoming HTTP connections on port 7100
+});
 #endregion
 
 #region Add services to the container.
@@ -21,16 +21,6 @@ builder.Services.AddApplicationServices(builder.Configuration, builder.Environme
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddRepositoryServices();
 #endregion
-
-if (builder.Environment.IsProduction())
-{
-    // Production Azure: If no WEBSITE_PORT exists set to 8080
-    var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
-    builder.WebHost.ConfigureKestrel(serverOptions =>
-    {
-        serverOptions.ListenAnyIP(int.Parse(port));
-    });
-}
 
 #region Configure the HTTP request pipeline.
 var app = builder.Build();
