bce8382a2c8fc989d826193e1b6d6e0c5277b8de
Reza Taba <mrtabaa@gmail.com>
2024-05-02 12:17:49 +0200

    GetAllAsync() return to nullable to check if get users doesn't fail

--- backend/api/Interfaces/IMemberRepository.cs
+++ backend/api/Interfaces/IMemberRepository.cs
@@ -2,7 +2,7 @@ namespace api.Interfaces;
 
 public interface IMemberRepository
 {
-    public Task<PagedList<AppUser>> GetAllAsync(MemberParams userParams, CancellationToken cancellationToken);
+    public Task<PagedList<AppUser>?> GetAllAsync(MemberParams userParams, CancellationToken cancellationToken);
 
     public Task<MemberDto?> GetByIdAsync(ObjectId? memberId, CancellationToken cancellationToken);
 
