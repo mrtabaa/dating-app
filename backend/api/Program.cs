029fbfb16a0531f5de1ccf242d54e87236d954fb
Reza Taba <mrtabaa@gmail.com>
2024-05-06 14:08:06 +0200

    Removed UseStaticFiles() for Azure storage

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -28,8 +28,6 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsProduction()) // https for production only
     app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-
 app.UseCors();
 
 app.UseAuthentication();
