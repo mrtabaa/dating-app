13a223e260294345740a1ca02a059e3e003968e9
Reza Taba <mrtabaa@gmail.com>
2024-05-06 15:42:51 +0200

    Added LoginDemo and Recover components

---
+++ client/src/app/components/account/recover/recover.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover',
+  standalone: true,
+  imports: [],
+  templateUrl: './recover.component.html',
+  styleUrl: './recover.component.scss'
+})
+export class RecoverComponent {
+
+}
