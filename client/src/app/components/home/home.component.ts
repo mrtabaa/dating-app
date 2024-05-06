63c777b6be4001f576cf13551081f6eba5d76b46
Reza Taba <mrtabaa@gmail.com>
2024-05-06 17:30:39 +0300

    Completed home page design

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { LoginComponent } from '../account/login/login.component';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import { LoginComponent } from '../account/login/login.component';
   imports: [
     LoginComponent,
     RouterLink,
-    MatButtonModule
+    MatButtonModule, MatDivider
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
