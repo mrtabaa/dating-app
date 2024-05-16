a308145e9b97efa5bac00132752440c6624b1844
Reza Taba <mrtabaa@gmail.com>
2024-05-16 19:41:13 +0300

    Query cannot be null. Make query non-nullable

--- backend/api/Helpers/PagedList.cs
+++ backend/api/Helpers/PagedList.cs
@@ -25,7 +25,7 @@ public class PagedList<T> : List<T>
     /// <param name="pageSize"></param>
     /// <param name="cancellationToken"></param>
     /// <returns>PageList<T> object with its prop values</returns>
-    public static async Task<PagedList<T>> CreatePagedListAsync(IMongoQueryable<T>? query, int pageNumber, int pageSize, CancellationToken cancellationToken)
+    public static async Task<PagedList<T>> CreatePagedListAsync(IMongoQueryable<T> query, int pageNumber, int pageSize, CancellationToken cancellationToken)
     {
         int count = await query.CountAsync(cancellationToken);
         IEnumerable<T> items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
