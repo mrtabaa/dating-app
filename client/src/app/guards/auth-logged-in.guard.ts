89fd0660b5d39720801f55db19e9e8277ac0333b
Reza Taba <mrtabaa@gmail.com>
2024-05-17 16:50:49 +0300

    Removed unnecessary routing. It's done once in AccountService

--- client/src/app/guards/auth-logged-in.guard.ts
+++ client/src/app/guards/auth-logged-in.guard.ts
@@ -1,14 +1,12 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const authLoggedInGuard: CanActivateFn = () => {
-  const router = inject(Router);
   const snackbar = inject(MatSnackBar);
 
   if (localStorage.getItem('loggedInUser')) {
     snackbar.open('You are already logged in.', 'Close', { horizontalPosition: 'center', verticalPosition: 'top', duration: 7000 });
-    router.navigate(['members']);
 
     return false;
   }
