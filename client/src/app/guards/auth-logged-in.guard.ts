4311792f37b6d83018ae4061ed265f63a7122407
Reza Taba <mrtabaa@gmail.com>
2024-05-22 12:54:36 +0200

    Route to complete-profile if page refreshed on home page on incomplete profiles

--- client/src/app/guards/auth-logged-in.guard.ts
+++ client/src/app/guards/auth-logged-in.guard.ts
@@ -1,12 +1,21 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoggedInUser } from '../models/logged-in-user.model';
 
 export const authLoggedInGuard: CanActivateFn = () => {
   const snackbar = inject(MatSnackBar);
+  const router = inject(Router);
 
-  if (localStorage.getItem('loggedInUser')) {
+  const loggedInUserStr: string | null = localStorage.getItem('loggedInUser');
+
+  if (loggedInUserStr) {
     snackbar.open('You are already logged in.', 'Close', { horizontalPosition: 'center', verticalPosition: 'top', duration: 7000 });
+
+    const loggedInUser: LoggedInUser = JSON.parse(loggedInUserStr);
+
+    if (!loggedInUser.isProfileCompleted)
+      router.navigate(['account/complete-profile']);
 
     return false;
   }
