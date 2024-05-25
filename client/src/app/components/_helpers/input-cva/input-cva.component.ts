22b5f4eb00dd7654122f1314173b09b1d3ce6a87
Reza Taba <mrtabaa@gmail.com>
2024-05-25 14:49:51 +0200

    Fixed AutoFocus directive

--- client/src/app/components/_helpers/input-cva/input-cva.component.ts
+++ client/src/app/components/_helpers/input-cva/input-cva.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input, Self } from '@angular/core';
 import { ControlValueAccessor, NgControl, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { AutofocusDirective } from '../../../directives/autofocus.directive';
 
 @Component({
   selector: 'app-input-cva',
   standalone: true,
   imports: [
+    AutofocusDirective,
     FormsModule, ReactiveFormsModule,
-    MatInputModule
+    MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule
   ],
   templateUrl: './input-cva.component.html',
   styleUrls: ['./input-cva.component.scss']
@@ -19,6 +24,8 @@ export class InputCvaComponent implements ControlValueAccessor {
   @Input() error = '';
   @Input() hint = '';
   @Input() isFocused = false;
+
+  isPasswordHidden = true;
 
   constructor(@Self() public ngControl: NgControl) {
     ngControl.valueAccessor = this;
@@ -36,5 +43,9 @@ export class InputCvaComponent implements ControlValueAccessor {
 
   get Control(): FormControl {
     return this.ngControl.control as FormControl;
+  }
+
+  showHidePassword(): void {
+    this.isPasswordHidden = !this.isPasswordHidden;
   }
 }
