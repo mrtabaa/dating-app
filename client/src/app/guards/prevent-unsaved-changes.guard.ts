c2fefc2d30bbc5898f4fc847299da86886c35431
Reza Taba <mrtabaa@gmail.com>
2024-05-29 21:46:58 +0300

    Improved prevent-unsaved-changes on leaving editd forms in a page

--- client/src/app/guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/guards/prevent-unsaved-changes.guard.ts
@@ -4,12 +4,14 @@ import { inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { map } from 'rxjs';
 import { ConfirmComponent } from '../components/_modals/confirm/confirm.component';
+import { CommonService } from '../services/common.service';
 
 // TODO Use it for User Management
-export const preventUnsavedChangesGuard: CanDeactivateFn<UserEditComponent> = (component) => {
+export const preventUnsavedChangesGuard: CanDeactivateFn<UserEditComponent> = () => {
   const dialog = inject(MatDialog);
+  const commonService = inject(CommonService);
 
-  if (component.userEditFg?.dirty) {
+  if (commonService.isPreventingLeavingPage) {
     const dialogRef = dialog.open(ConfirmComponent);
 
     return dialogRef.afterClosed()
@@ -21,7 +23,6 @@ export const preventUnsavedChangesGuard: CanDeactivateFn<UserEditComponent> = (c
         }
         )
       )
-
   }
   return true;
 };
