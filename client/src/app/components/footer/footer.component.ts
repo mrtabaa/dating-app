4ab347b940ee68c3cdf5d3e1869271cde5f2bb32
Reza Taba <mrtabaa@gmail.com>
2024-06-05 19:11:10 +0300

    Made footer reactive based on userLoggedIn and Mobile/desktop view

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ResponsiveService } from '../../services/responsive.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  responsiveService = inject(ResponsiveService);
 
+  @Input() isLoggedInIn: boolean = false;
 }
---
+++ client/src/app/services/responsive.service.ts
@@ -0,0 +1,8 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponsiveService {
+  isMobileSig = signal<boolean>(false);
+}
