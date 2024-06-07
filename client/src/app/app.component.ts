d708b0f272e3dc6b5764114c45964c7420d1d4c7
Reza Taba <mrtabaa@gmail.com>
2024-06-07 16:01:20 +0300

    Separated navbar desktop from mobile. Handle responsive on app.component instead

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,13 +7,17 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserService } from './services/user.service';
 import { FooterComponent } from './components/footer/footer.component';
 import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Observable, map } from 'rxjs';
+import { ResponsiveService } from './services/responsive.service';
+import { NavMobileComponent } from './components/navbar/nav-mobile/nav-mobile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, RouterOutlet, NgxSpinnerModule,
-    NavbarComponent, FooterComponent, WelcomeComponent
+    NavbarComponent, NavMobileComponent, FooterComponent, WelcomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
@@ -22,6 +26,23 @@ export class AppComponent implements OnInit {
   accountService = inject(AccountService);
   userService = inject(UserService);
   router = inject(Router);
+  private breakpointObserver = inject(BreakpointObserver);
+  loggedInUserSig = inject(AccountService).loggedInUserSig;
+  isMobileSig = inject(ResponsiveService).isMobileSig;
+  isWelcomeCompSig = inject(ResponsiveService).isWelcomeCompSig;
+
+  isMobileView$: Observable<boolean>;
+
+  constructor() {
+    this.isMobileView$ = this.breakpointObserver.observe('(min-width: 51rem)') // include iPad/tablet
+      .pipe(map(({ matches }) => {
+        matches = matches ? false : true
+
+        this.isMobileSig.set(matches);
+
+        return matches;
+      }));
+  }
 
   title = 'Hallboard';
   isLoading: boolean = false;
