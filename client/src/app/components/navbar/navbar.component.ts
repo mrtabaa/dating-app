8379606012bafb7c8a8864838324937b24fdfa31
Reza Taba <mrtabaa@gmail.com>
2024-06-07 16:01:20 +0300

    Separated navbar desktop from mobile. Handle responsive on app.component instead

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -9,17 +9,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
-import { ResponsiveService } from '../../services/responsive.service';
-import { NavMobileComponent } from './nav-mobile/nav-mobile.component';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     CommonModule, RouterLink, RouterLinkActive, NgOptimizedImage,
-    NavMobileComponent,
     MatIconModule, MatToolbarModule, MatTabsModule, MatMenuModule,
     MatButtonModule, MatListModule, MatDividerModule
   ],
@@ -29,35 +24,12 @@ import { Observable, map } from 'rxjs';
 export class NavbarComponent {
   private router = inject(Router);
   private accountService = inject(AccountService);
-  private breakpointObserver = inject(BreakpointObserver);
   loggedInUserSig = inject(AccountService).loggedInUserSig;
-  isMobileSig = inject(ResponsiveService).isMobileSig;
-  isWelcomeCompSig = inject(ResponsiveService).isWelcomeCompSig;
-
-  isMobileView$: Observable<boolean>;
-
-  constructor() {
-    this.isMobileView$ = this.breakpointObserver.observe('(min-width: 51rem)') // include iPad
-      .pipe(map(({ matches }) => {
-        matches = matches ? false : true
-
-        this.isMobileSig.set(matches);
-
-        return matches;
-      }));
-  }
 
   links = ['members', 'friends', 'messages', 'admin'];
 
   goToEditProfile(): void {
     this.router.navigate(['member/' + this.accountService.loggedInUserSig()?.userName], { skipLocationChange: true });
-  }
-
-  /**
-   * Set isWelcomeCompSig to true to show WelcomeComponent if brand is clicked. 
-   */
-  setIsWelcomeCompSig(): void {
-    this.isWelcomeCompSig.set(true);
   }
 
   logout() {
