dbeb0c2ae208351606c2a4037ca3dea369de51f9
Reza Taba <mrtabaa@gmail.com>
2024-06-07 16:27:58 +0300

    Implemented NavMobile if not logged in

--- client/src/app/components/navbar/nav-mobile/nav-mobile.component.ts
+++ client/src/app/components/navbar/nav-mobile/nav-mobile.component.ts
@@ -1,18 +1,31 @@
-import {MatSidenavModule} from '@angular/material/sidenav'; 
-import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { ResponsiveService } from '../../../services/responsive.service';
+import { CommonModule } from '@angular/common';
+import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-nav-mobile',
   standalone: true,
   imports: [
+    RouterModule, CommonModule,
     MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule
   ],
   templateUrl: './nav-mobile.component.html',
   styleUrl: './nav-mobile.component.scss'
 })
 export class NavMobileComponent {
+  loggedInUserSig = inject(AccountService).loggedInUserSig;
+  isWelcomeCompSig = inject(ResponsiveService).isWelcomeCompSig;
 
+  /**
+     * Set isWelcomeCompSig to true to show WelcomeComponent if brand is clicked. 
+     */
+  setIsWelcomeCompSig(): void {
+    this.isWelcomeCompSig.set(true);
+  }
 }
