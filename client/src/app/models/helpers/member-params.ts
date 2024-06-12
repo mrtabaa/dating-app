6a28a20acab782527984cd1086e9b145d7bb9e08
Reza Taba <mrtabaa@gmail.com>
2024-06-12 08:19:53 +0300

    Fixed the comment and clean code

--- client/src/app/models/helpers/member-params.ts
+++ client/src/app/models/helpers/member-params.ts
@@ -1,13 +1,13 @@
 import { BaseParams } from "./BaseParams";
 
-export class MemberParams extends BaseParams{
+export class MemberParams extends BaseParams {
     gender: string;
     minAge = 18;
     maxAge = 99;
     orderBy = 'lastActive';
 
     constructor(gender: string) {
-        super(); // Constructors for derived classes must contain a 'super' call.
+        super(); // Constructors of derived classes must contain a 'super' call to properly set up the inheritance chain.
         this.gender = gender === 'female' ? 'male' : 'female';
     }
 }
