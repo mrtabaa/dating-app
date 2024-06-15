8e8de984c00b1d360f62697412026cabb6c2f3d0
Reza Taba <mrtabaa@gmail.com>
2024-06-15 11:31:33 +0300

    Fixed new member$ details not being updated after 'member/:userName' route is changed

--- client/src/app/components/members/member-detail/member-detail-mobile/member-detail-mobile.component.ts
+++ client/src/app/components/members/member-detail/member-detail-mobile/member-detail-mobile.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
 import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { Observable, Subscription, take } from 'rxjs';
+import { Observable, Subscription, of, switchMap, take } from 'rxjs';
 import { ApiResponseMessage } from '../../../../models/helpers/api-response-message';
 import { Member } from '../../../../models/member.model';
 import { AccountService } from '../../../../services/account.service';
@@ -29,7 +29,7 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './member-detail-mobile.component.html',
   styleUrl: './member-detail-mobile.component.scss'
 })
-export class MemberDetailMobileComponent implements OnInit, OnDestroy {
+export class MemberDetailMobileComponent implements OnInit {
   private memberService = inject(MemberService);
   private followService = inject(FollowService);
   username = inject(AccountService).loggedInUserSig()?.userName;
@@ -38,26 +38,35 @@ export class MemberDetailMobileComponent implements OnInit, OnDestroy {
   private gallery = inject(Gallery);
   router = inject(Router);
 
-  member$: Observable<Member> | undefined;
+  member$: Observable<Member | null> | undefined;
   subscribed: Subscription | undefined;
   images: GalleryItem[] = [];
   readonly imageWH = 50;
 
   ngOnInit(): void {
     this.getMember();
-    this.setGalleryImages();
-  }
-
-  ngOnDestroy(): void {
-    this.subscribed;
   }
 
   getMember(): void {
-    const userName: string | null = this.route.snapshot.paramMap.get('userName');
+    // Basic way: snapshot takes current URL only once. Angular doesn't detect if another userName(URL) is entered to refresh the MemberDetails for the new URL.
+    // const userName: string | null = this.route.snapshot.paramMap.get('userName'); 
 
-    if (userName) {
-      this.member$ = this.memberService.getMemberByUsername(userName);
-    }
+    // Advance way: params.pipe makes Angular detect if the URL is changed to a new userName(URL change) and gets the new Member to update the DOM
+    this.member$ = this.route.params.pipe(
+      switchMap((params: Params): Observable<Member | null> => {
+        const userName = params['userName'];
+        if (!userName) {
+          // Return an Observable that emits null
+          return of(null);
+        }
+        // Call the service and ensure that an Observable is always returned
+        const memberObservable = this.memberService.getMemberByUsername(userName);
+        return memberObservable ? memberObservable : of(null);
+      })
+    );
+
+    if (this.member$ !== of(null))
+      this.setGalleryImages();
   }
 
   addFollow(member: Member): void {
@@ -91,16 +100,17 @@ export class MemberDetailMobileComponent implements OnInit, OnDestroy {
   }
 
   setGalleryImages(): void {
-    this.subscribed = this.member$?.subscribe(
-      (member: Member) => {
+    this.subscribed = this.member$?.subscribe((member: Member | null) => {
+      this.images = []; // reset images for new member$ before loading in galleryRef
+      
+      if (member)
         for (const photo of member.photos) {
           this.images.push(new ImageItem({ src: photo.url_enlarged, thumb: photo.url_165 }));
         }
 
-        // load ng-gallery and insert images
-        const galleryRef = this.gallery.ref();
-        galleryRef.load(this.images)
-      }
-    );
+      // load ng-gallery and insert images
+      const galleryRef = this.gallery.ref();
+      galleryRef.load(this.images)
+    });
   }
 }
