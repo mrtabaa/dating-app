79431801a50635c53f5dd2ff5af38f459e3fc603
Reza Taba <mrtabaa@gmail.com>
2024-06-26 16:23:45 +0300

    Changed api from Azure to AWS

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://da-api-mr.azurewebsites.net/api/',
-  apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/',
+  apiUrl: 'http://da-api-prod.us-east-1.elasticbeanstalk.com/api/',
+  apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/'
+  // apiUrl: 'https://da-api-mr.azurewebsites.net/api/',
+  // apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/',
 };
