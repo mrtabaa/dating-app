16d8939fad529bb756f3c24bea86d51dff35b33b
Reza Taba <mrtabaa@gmail.com>
2024-06-26 16:48:56 +0300

    Removed turnstile

--- backend/api/Extensions/IdentityServiceExtensions.cs
+++ backend/api/Extensions/IdentityServiceExtensions.cs
@@ -1,16 +1,9 @@
-using AspNetCore.Identity.MongoDbCore.Extensions;
-using AspNetCore.Identity.MongoDbCore.Infrastructure;
-
 namespace api.Extensions;
 
 public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
-        #region Turnstile Token Validation
-        services.AddHttpClient<Interfaces.IRecaptchaService, Services.RecaptchaService>();
-        #endregion
-
         #region Token
         string? tokenValue = config.GetValue<string>(AppVariablesExtensions.TokenKey);
 
