b96d93d0aeb5bba847f17aee0d47e803122dc89b
Reza Taba <mrtabaa@gmail.com>
2024-06-26 23:39:28 +0300

    Use api sub-domain

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://da-api-prod.us-east-1.elasticbeanstalk.com/api/',
+  apiUrl: 'https://api.hallboard.com/api/',
   apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/'
   // apiUrl: 'https://da-api-mr.azurewebsites.net/api/',
   // apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/',
