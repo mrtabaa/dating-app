c5801e14ed178062b492a5c7260955a0632f5c48
Reza Taba <mrtabaa@gmail.com>
2024-06-27 00:07:15 +0300

    Removed Kestrel porting

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -9,16 +9,6 @@ builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddRepositoryServices();
 #endregion
 
-if (builder.Environment.IsProduction())
-{
-    // Production Azure: If no WEBSITE_PORT exists set to 8080
-    var port = Environment.GetEnvironmentVariable("WEBSITE_PORT") ?? "8080";
-    builder.WebHost.ConfigureKestrel(serverOptions =>
-    {
-        serverOptions.ListenAnyIP(int.Parse(port)); // TODO set to an allowed ip only
-    });
-}
-
 #region Configure the HTTP request pipeline.
 var app = builder.Build();
 
