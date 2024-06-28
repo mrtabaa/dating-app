d182fb86d4a912714985874e9b81aa8745a64ab5
Reza Taba <mrtabaa@gmail.com>
2024-06-28 18:57:23 +0300

    Added under development massage

--- client/src/app/components/account/recover/recover.component.ts
+++ client/src/app/components/account/recover/recover.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recover',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    MatButtonModule
+  ],
   templateUrl: './recover.component.html',
   styleUrl: './recover.component.scss'
 })
