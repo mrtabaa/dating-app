4d34010681a1cc08fd1c76abdcb946d06049fef3
Reza Taba <mrtabaa@gmail.com>
2024-07-10 23:33:14 +0200

    Fixed Create Message in Repo

--- backend/api/Models/Helpers/MessageStatus.cs
+++ backend/api/Models/Helpers/MessageStatus.cs
@@ -2,5 +2,6 @@ namespace api.Models.Helpers;
 
 public record MessageStatus(
     [Optional] bool IsReceiverNotFound,
+    [Optional] bool IsLoggedInUserNotFound,
     [Optional] MessageDto MessageDto
 );
