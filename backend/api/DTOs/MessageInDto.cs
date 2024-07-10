7e5690413bcf04292e58110fdc6702f7d2d69c40
Reza Taba <mrtabaa@gmail.com>
2024-07-10 21:09:07 +0200

    Set Length for inputs

--- backend/api/DTOs/MessageInDto.cs
+++ backend/api/DTOs/MessageInDto.cs
@@ -1,6 +1,6 @@
 namespace api.DTOs;
 
 public record MessageInDto(
-    string Content,
-    string ReceiverUserName
+    [Length(1, 500)] string Content,
+    [Length(1, 50)] string ReceiverUserName
 );
