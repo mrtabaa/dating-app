b52e46d9db90a9c048faadcb091142ec79b99e17
Reza Taba <mrtabaa@gmail.com>
2024-07-18 16:44:00 +0300

    Made scroll cdk's itemSize dynamic using CdkDynamicHeightDirective

---
+++ client/src/app/directives/cdk-dynamic-height.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CdkDynamicHeightDirective } from './cdk-dynamic-height.directive';
+
+describe('CdkDynamicHeightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CdkDynamicHeightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
