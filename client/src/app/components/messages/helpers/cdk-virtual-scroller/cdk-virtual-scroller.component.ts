6042ed468e619ac94c386a96440251f7ba0908f0
Reza Taba <mrtabaa@gmail.com>
2024-07-26 22:01:23 +0200

    Implemented first layout and reading 1st page data

--- client/src/app/components/messages/helpers/cdk-virtual-scroller/cdk-virtual-scroller.component.ts
+++ client/src/app/components/messages/helpers/cdk-virtual-scroller/cdk-virtual-scroller.component.ts
@@ -1,29 +1,33 @@
 import { CdkVirtualScrollViewport, ScrollingModule } from '@angular/cdk/scrolling';
-import { AfterViewChecked, Component, Input, ViewChild } from '@angular/core';
-import { Message } from '../../../../models/message.model';
+import { Component, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Member } from '../../../../models/member.model';
+import { Message } from '../../../../models/message.model';
+import { ShortenStringPipe } from '../../../../pipes/shorten-string.pipe';
+import { ResponsiveService } from '../../../../services/responsive.service';
 
 @Component({
   selector: 'app-cdk-virtual-scroller',
   standalone: true,
   imports: [
     CommonModule, NgOptimizedImage,
-    ScrollingModule
+    ScrollingModule, ShortenStringPipe
   ],
   templateUrl: './cdk-virtual-scroller.component.html',
   styleUrl: './cdk-virtual-scroller.component.scss'
 })
-export class CdkVirtualScrollerComponent implements AfterViewChecked {
-  ngAfterViewChecked(): void {
-    if (this.messagesIn)
-      console.log(this.messagesIn);
-  }
+export class CdkVirtualScrollerComponent implements OnInit {
   @ViewChild(CdkVirtualScrollViewport) private viewport: CdkVirtualScrollViewport | undefined;
   @Input() messagesIn: Message[] | undefined;
-  @Input() memberIn: Member | undefined;
+  @Input() pageSizeIn: number | undefined;
+  isMobileSig = inject(ResponsiveService).isMobileSig;
   photo_WH = 40;
+  defaultItemSize = 50;
   bufferSize = 0;
+
+  ngOnInit(): void {
+    if (this.pageSizeIn)
+      this.bufferSize = this.defaultItemSize * this.pageSizeIn
+  }
 
   loadOlderMessages(event: number): void {
     if (this.viewport) {
