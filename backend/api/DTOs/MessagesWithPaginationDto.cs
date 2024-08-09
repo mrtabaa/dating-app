b1577213cfaede57a096e5ecad577d8651920480
Reza Taba <mrtabaa@gmail.com>
2024-08-09 21:23:51 +0200

    Fixed mapping between api DTO and client model

--- backend/api/DTOs/MessagesWithPaginationDto.cs
+++ backend/api/DTOs/MessagesWithPaginationDto.cs
@@ -2,7 +2,7 @@ namespace api.DTOs;
 
 public class MessagesWithPaginationDto
 {
-    public IEnumerable<MessageDto> MessageDtos { get; set; } = [];
-    public PaginationHeader? PaginationHeader { get; set; }
+    public IEnumerable<MessageDto> Messages { get; set; } = [];
+    public PaginationHeader? Pagination { get; set; }
 
 }
