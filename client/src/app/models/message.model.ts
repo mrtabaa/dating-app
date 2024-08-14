9c02ec0d9e01ba78ba01e9f22c12923277a1f33c
Reza Taba <mrtabaa@gmail.com>
2024-08-14 19:14:50 +0300

    Reordered props

--- client/src/app/models/message.model.ts
+++ client/src/app/models/message.model.ts
@@ -1,6 +1,6 @@
 export interface Message {
-    tempId?: string; // Must be optional to be deleted after its work is done
     Id?: string;
+    tempId?: string; // Must be optional to be deleted after its work is done
     userOrTargetUserName: string | undefined;
     userOrTargetKnownAs: string | undefined;
     userOrTargetProfilePhoto: string | undefined;
