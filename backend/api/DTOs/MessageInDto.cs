73f4d2b9f41a5c3a20552d3bfd8247d56cc401a5
Reza Taba <mrtabaa@gmail.com>
2024-08-15 21:17:39 +0300

    Removed unnecessary attribute

--- backend/api/DTOs/MessageInDto.cs
+++ backend/api/DTOs/MessageInDto.cs
@@ -3,5 +3,5 @@ namespace api.DTOs;
 public record MessageInDto(
     string TempId,
     [Length(1, 500)] string Content,
-    [Length(1, 50)] string ReceiverUserName
+    string ReceiverUserName
 );
