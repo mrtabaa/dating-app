2bdbec989357a1904ff635e2bb6a37940c1bcd57
Reza Taba <mrtabaa@gmail.com>
2024-08-31 23:46:32 +0200

    Reverted props to normal

--- backend/api/Models/Errors/ApiException.cs
+++ backend/api/Models/Errors/ApiException.cs
@@ -7,8 +7,4 @@ public class ApiException
     public string? Message { get; set; }
     public string? Details { get; set; }
     public DateTime Time { get; set; }
-    public int Number { get; set; }
-    public MessageInDto? MessageInDto { get; set; }
-    public MessageDto? MessageDto { get; set; }
-    public ObjectId? UserId { get; set; }
 };
