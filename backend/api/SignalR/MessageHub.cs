a216caf15802b3ea837227cda068561d42bbc93e
Reza Taba <mrtabaa@gmail.com>
2024-08-31 23:14:08 +0200

    Save log if messageDto is null

--- backend/api/SignalR/MessageHub.cs
+++ backend/api/SignalR/MessageHub.cs
@@ -47,23 +47,24 @@ public class MessageHub : Hub
         HttpContext httpContext = Context.GetHttpContext()
             ?? throw new HubException("httpContext cannot be null!");
 
-        SaveLog(1, httpContext, null, messageInDto);
+        await SaveLogAsync(1, httpContext, null, messageInDto);
 
         CancellationToken cancellationToken = httpContext.RequestAborted;
 
         ObjectId userId = await _tokenService.GetActualUserIdAsync(Context.User?.GetUserIdHashed(), cancellationToken)
             ?? throw new HubException("User id is invalid. Login again.");
 
-        SaveLog(2, httpContext, userId, messageInDto);
+        await SaveLogAsync(2, httpContext, userId, messageInDto);
 
-        MessageDto messageDto = await _messageRepository.CreateAsync(userId, messageInDto, cancellationToken)
-            ?? throw new HubException("Message creation failed. Try again.");
+        MessageDto? messageDto = await _messageRepository.CreateAsync(userId, messageInDto, cancellationToken);
+        // ?? throw new HubException("Message creation failed. Try again.");
 
-        SaveLog(3, httpContext, userId, messageInDto, messageDto);
+        if (messageDto is null)
+            await SaveLogAsync(3, httpContext, userId, messageInDto, messageDto);
 
         string groupName = GetGroupName(GetUserName(), messageInDto.ReceiverUserName.ToUpper());
 
-        SaveLog(4, httpContext, userId, messageInDto, messageDto, groupName);
+        await SaveLogAsync(4, httpContext, userId, messageInDto, messageDto, groupName);
 
         await Clients.Group(groupName).SendAsync(NewMessageRes, messageDto, cancellationToken);
     }
@@ -78,7 +79,7 @@ public class MessageHub : Hub
     private string GetUserName() =>
         Context.User?.GetUserName() ?? throw new HubException("UserName is invalid. Login again.");
 
-    private async void SaveLog(int number, HttpContext context, ObjectId? userId = null, MessageInDto? messageInDto = null, MessageDto? messageDto = null, string? groupName = null)
+    private async Task SaveLogAsync(int number, HttpContext context, ObjectId? userId = null, MessageInDto? messageInDto = null, MessageDto? messageDto = null, string? groupName = null)
     {
         ApiException response = new()
         {
