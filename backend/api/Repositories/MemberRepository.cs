db289601d61930fa37c78b90897a20a05e8c9af6
Reza Taba <mrtabaa@gmail.com>
2024-11-22 17:12:31 +0100

    Improved format and spelling

--- backend/api/Repositories/MemberRepository.cs
+++ backend/api/Repositories/MemberRepository.cs
@@ -3,51 +3,73 @@ namespace api.Repositories;
 public class MemberRepository : IMemberRepository
 {
     private readonly IMongoCollection<AppUser>? _collection;
-    private readonly IPhotoService _photoService;
     private readonly IFollowRepository _followRepository;
+    private readonly IPhotoService _photoService;
+
     #region Db and Token Settings
 
     // constructor - dependency injections
     public MemberRepository(IMongoClient client, IMyMongoDbSettings dbSettings, IPhotoService photoService, IFollowRepository followRepository)
     {
-        IMongoDatabase? dbName = client.GetDatabase(dbSettings.DatabaseName) ?? throw new ArgumentNullException(nameof(dbName));
+        IMongoDatabase dbName = client.GetDatabase(dbSettings.DatabaseName) ?? throw new ArgumentNullException(nameof(dbName));
         _collection = dbName.GetCollection<AppUser>(AppVariablesExtensions.collectionUsers);
         _photoService = photoService;
         _followRepository = followRepository;
     }
+
+    #endregion
+
+    #region Helpers
+
+    private AppUser? ConvertAppUserPhotosToBlobPhotos(AppUser appUser)
+    {
+        List<Photo>? blobConvertedPhotos = _photoService.ConvertAllPhotosToBlobLinkWithSas(appUser.Photos)?.ToList();
+        if (blobConvertedPhotos is null)
+            return null;
+
+        appUser.Photos = blobConvertedPhotos;
+
+        return appUser;
+    }
+
     #endregion
 
     #region CRUD
+
     public async Task<PagedList<AppUser>?> GetPagedListAsync(MemberParams memberParams, CancellationToken cancellationToken)
     {
         // For small lists
         // var appUsers = await _collection.Find<AppUser>(new BsonDocument()).ToListAsync(cancellationToken);
 
         #region Filters
-        // calculate DOB based on user's selected Age
-        var MinDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MaxAge - 1));
-        var MaxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MinAge));
 
-        // set query to AsQuerable to use it agains MongoDB later
+        // calculate DOB based on user's selected Age
+        DateOnly minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MaxAge - 1));
+        DateOnly maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MinAge));
+
+        // set query to AsQuerable to use it again MongoDB later
         IMongoQueryable<AppUser> query = _collection.AsQueryable();
 
         query = query.Where(appUser => appUser.Id != memberParams.UserId); // don't request/show the currentUser in the list
         query = query.Where(appUser => !(appUser.NormalizedUserName == "ADMIN" || appUser.UserName == "MODERATOR")); // don't show admin/moderator
         query = query.Where(appUser => !(string.IsNullOrEmpty(appUser.NormalizedUserName) || appUser.NormalizedUserName.StartsWith("DEMO"))); // don't show demo users
         query = query.Where(appUser => appUser.Gender == memberParams.Gender); // get the opposite gender by default. It's set in MemberController
-        query = query.Where(appUser => appUser.DateOfBirth >= MinDob && appUser.DateOfBirth <= MaxDob); // get ages between 2 age inputs
+        query = query.Where(appUser => appUser.DateOfBirth >= minDob && appUser.DateOfBirth <= maxDob); // get ages between 2 age inputs
         query = memberParams.OrderBy switch
-        { // sort users based on Age, Created or LastActive
+        {
+            // sort users based on Age, Created or LastActive
             "age" => query.OrderByDescending(appUser => appUser.DateOfBirth).ThenBy(appUser => appUser.Id),
             "created" => query.OrderByDescending(appUser => appUser.CreatedOn).ThenBy(appUser => appUser.Id),
             _ => query.OrderByDescending(appUser => appUser.LastActive).ThenBy(appUser => appUser.Id)
         };
+
         #endregion Filters
 
         PagedList<AppUser> appUsers = await PagedList<AppUser>.CreatePagedListAsync(query, memberParams.PageNumber, memberParams.PageSize, cancellationToken);
 
         #region Convert all members' appUser.Photos to BlobLinkFormat
-        for (int i = 0; i < appUsers.Count; i++)
+
+        for (var i = 0; i < appUsers.Count; i++)
         {
             AppUser? appUser = null;
 
@@ -60,6 +82,7 @@ public class MemberRepository : IMemberRepository
                 appUsers[i] = appUser;
             }
         }
+
         #endregion Convert all members' appUser.Photos to BlobLinkFormat
 
         return appUsers;
@@ -67,15 +90,15 @@ public class MemberRepository : IMemberRepository
 
     public async Task<MemberDto?> GetByIdAsync(ObjectId userId, ObjectId? memberId, CancellationToken cancellationToken)
     {
-        AppUser? appUser = await _collection.Find<AppUser>(appUser => appUser.Id == memberId).FirstOrDefaultAsync(cancellationToken);
+        AppUser? appUser = await _collection.Find(appUser => appUser.Id == memberId).FirstOrDefaultAsync(cancellationToken);
 
         appUser = ConvertAppUserPhotosToBlobPhotos(appUser);
 
         return appUser is null
             ? null
             : await _followRepository.CheckIsFollowing(userId, appUser, cancellationToken)
-            ? Mappers.ConvertAppUserToMemberDto(appUser, isFollowing: true)
-            : Mappers.ConvertAppUserToMemberDto(appUser);
+                ? Mappers.ConvertAppUserToMemberDto(appUser, true)
+                : Mappers.ConvertAppUserToMemberDto(appUser);
     }
 
     public async Task<IEnumerable<AppUser>> GetAppUsersByIdsAsync(IEnumerable<ObjectId> membersIds, CancellationToken cancellationToken) =>
@@ -83,26 +106,16 @@ public class MemberRepository : IMemberRepository
 
     public async Task<MemberDto?> GetByUserNameAsync(ObjectId userId, string userName, CancellationToken cancellationToken)
     {
-        AppUser? appUser = await _collection.Find<AppUser>(appUser => appUser.NormalizedUserName == userName.ToUpper().Trim()).FirstOrDefaultAsync(cancellationToken);
+        AppUser? appUser = await _collection.Find(appUser => appUser.NormalizedUserName == userName.ToUpper().Trim()).FirstOrDefaultAsync(cancellationToken);
 
         appUser = ConvertAppUserPhotosToBlobPhotos(appUser);
 
         return appUser is null
             ? null
             : await _followRepository.CheckIsFollowing(userId, appUser, cancellationToken)
-            ? Mappers.ConvertAppUserToMemberDto(appUser, isFollowing: true)
-            : Mappers.ConvertAppUserToMemberDto(appUser);
+                ? Mappers.ConvertAppUserToMemberDto(appUser, true)
+                : Mappers.ConvertAppUserToMemberDto(appUser);
     }
+
     #endregion CRUD
-
-    private AppUser? ConvertAppUserPhotosToBlobPhotos(AppUser appUser)
-    {
-        List<Photo>? blobConvertedPhotos = _photoService.ConvertAllPhotosToBlobLinkWithSas(appUser.Photos)?.ToList();
-        if (blobConvertedPhotos is null)
-            return null;
-
-        appUser.Photos = blobConvertedPhotos;
-
-        return appUser;
-    }
 }
