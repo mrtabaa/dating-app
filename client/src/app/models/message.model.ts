21c5044d51d6d3dd079abb9221bd35cf777972c0
Reza Taba <mrtabaa@gmail.com>
2024-12-20 12:46:12 +0100

    Added missing prop height needed in appCdkDynamicHeightDir

--- client/src/app/models/message.model.ts
+++ client/src/app/models/message.model.ts
@@ -1,10 +1,11 @@
 export interface Message {
-    Id?: string;
-    tempId?: string; // Must be optional to be deleted after its work is done
-    userOrTargetUserName: string | undefined;
-    userOrTargetKnownAs: string | undefined;
-    userOrTargetProfilePhoto: string | undefined;
-    content: string;
-    sentOn?: Date;
-    readOn?: Date;
+  Id?: string;
+  tempId?: string; // Must be optional to be deleted after its work is done
+  userOrTargetUserName: string | undefined;
+  userOrTargetKnownAs: string | undefined;
+  userOrTargetProfilePhoto: string | undefined;
+  content: string;
+  sentOn?: Date;
+  readOn?: Date;
+  height: number; // used in appCdkDynamicHeightDir to calculate a message height
 }
