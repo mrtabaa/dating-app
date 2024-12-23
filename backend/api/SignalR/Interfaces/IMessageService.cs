ced449bd08dcf14dffd3cae005b8c177aff9572f
Reza Taba <mrtabaa@gmail.com>
2024-12-23 19:28:00 +0100

    Improved name

--- backend/api/SignalR/Interfaces/IMessageService.cs
+++ backend/api/SignalR/Interfaces/IMessageService.cs
@@ -3,6 +3,6 @@ namespace api.SignalR.Interfaces;
 public interface IMessageService
 {
     public Task<bool> AddGroupNameAsync(ObjectId userId, string groupName, CancellationToken cancellationToken);
-    public Task<bool> CheckIsMemberInGroupAsync(ObjectId userId, string groupNameIn, CancellationToken cancellationToken);
+    public Task<bool> CheckIsMemberIsInGroupAsync(ObjectId userId, string groupNameIn, CancellationToken cancellationToken);
     public Task<bool> RemoveGroupNameFromDbAsync(ObjectId userId, string groupName, CancellationToken cancellationToken);
 }
