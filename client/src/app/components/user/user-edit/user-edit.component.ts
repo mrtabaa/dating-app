6bf58800df278e12e4fdd2b0adbdad6573484771
Reza Taba <mrtabaa@gmail.com>
2025-01-01 19:52:20 +0100

    Removed unnecessary Recaptcha

--- client/src/app/components/user/user-edit/user-edit.component.ts
+++ client/src/app/components/user/user-edit/user-edit.component.ts
@@ -1,68 +1,100 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Subscription, take } from 'rxjs';
-import { UserUpdate } from '../../../models/user-update.model';
-import { Member } from '../../../models/member.model';
-import { PhotoEditorComponent } from '../../user/photo-editor/photo-editor.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MemberService } from '../../../services/member.service';
-import { UserService } from '../../../services/user.service';
-import { IntlModule } from 'angular-ecmascript-intl';
-import { ApiResponseMessage } from '../../../models/helpers/api-response-message';
-import { LoggedInUser } from '../../../models/logged-in-user.model';
-import { AccountService } from '../../../services/account.service';
-import { DatePickerCvaComponent } from '../../_helpers/date-picker-cva/date-picker-cva.component';
-import { GooglePlacesComponent } from '../../google-places/google-places.component';
-import { GooglePlacesService } from '../../../services/google-places.service';
-import { CommonService } from '../../../services/common.service';
-import { ResponsiveService } from '../../../services/responsive.service';
-import { LoadingService } from '../../../services/loading.service';
-import { RecaptchaV3Module, ReCaptchaV3Service } from "ng-recaptcha";
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ShortenStringPipe } from '../../../pipes/shorten-string.pipe';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Subscription, take} from 'rxjs';
+import {UserUpdate} from '../../../models/user-update.model';
+import {Member} from '../../../models/member.model';
+import {PhotoEditorComponent} from '../photo-editor/photo-editor.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MemberService} from '../../../services/member.service';
+import {UserService} from '../../../services/user.service';
+import {IntlModule} from 'angular-ecmascript-intl';
+import {ApiResponseMessage} from '../../../models/helpers/api-response-message';
+import {LoggedInUser} from '../../../models/logged-in-user.model';
+import {AccountService} from '../../../services/account.service';
+import {DatePickerCvaComponent} from '../../_helpers/date-picker-cva/date-picker-cva.component';
+import {GooglePlacesComponent} from '../../google-places/google-places.component';
+import {GooglePlacesService} from '../../../services/google-places.service';
+import {CommonService} from '../../../services/common.service';
+import {ResponsiveService} from '../../../services/responsive.service';
+import {LoadingService} from '../../../services/loading.service';
+import {RecaptchaV3Module, ReCaptchaV3Service} from "ng-recaptcha";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {ShortenStringPipe} from '../../../pipes/shorten-string.pipe';
 
 @Component({
-    selector: 'app-user-edit',
-    imports: [
-        CommonModule, NgOptimizedImage, FormsModule, ReactiveFormsModule,
-        PhotoEditorComponent, DatePickerCvaComponent, GooglePlacesComponent,
-        RecaptchaV3Module,
-        MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSlideToggleModule,
-        IntlModule, ShortenStringPipe
-    ],
-    templateUrl: './user-edit.component.html',
-    styleUrls: ['./user-edit.component.scss']
+  selector: 'app-user-edit',
+  imports: [
+    CommonModule, NgOptimizedImage, FormsModule, ReactiveFormsModule,
+    PhotoEditorComponent, DatePickerCvaComponent, GooglePlacesComponent,
+    RecaptchaV3Module,
+    MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSlideToggleModule,
+    IntlModule, ShortenStringPipe
+  ],
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.scss']
 })
 export class UserEditComponent implements OnInit, OnDestroy {
-  private _userService = inject(UserService);
-  private _memberService = inject(MemberService);
-  private _accountService = inject(AccountService);
-  private _googlePlacesService = inject(GooglePlacesService);
-  private _commonService = inject(CommonService);
-  private _fb = inject(FormBuilder);
-  private _matSnak = inject(MatSnackBar);
   isMobileSig = inject(ResponsiveService).isMobileSig;
   isLoadingSig = inject(LoadingService).isLoadingsig;
-  private _recaptchaService = inject(ReCaptchaV3Service);
-  private _recaptchaToken: string | undefined;
-
-  private _userEditFgSubscribed: Subscription | undefined;
-  private _subscribedRecaptcha: Subscription | undefined;
-
   member: Member | undefined;
-
   minDate = new Date();
   maxDate = new Date();
   readonly minTextAreaChars: number = 10;
   readonly maxTextAreaChars: number = 1000;
   readonly minInputChars: number = 3;
   readonly maxInputChars: number = 30;
+  private _userService = inject(UserService);
+  private _memberService = inject(MemberService);
+  private _accountService = inject(AccountService);
+  private _googlePlacesService = inject(GooglePlacesService);
+  private _commonService = inject(CommonService);
+  private _fb = inject(FormBuilder);
+  userEditFg: FormGroup = this._fb.group({
+    knownAsCtrl: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
+    dateOfBirthCtrl: [null, [Validators.required]],
+    introductionCtrl: ['', [Validators.maxLength(this.maxTextAreaChars)]],
+    lookingForCtrl: ['', [Validators.maxLength(this.maxTextAreaChars)]],
+    interestsCtrl: ['', [Validators.maxLength(this.maxTextAreaChars)]]
+  });
+  private _matSnak = inject(MatSnackBar);
+  private _recaptchaService = inject(ReCaptchaV3Service);
+  private _recaptchaToken: string | undefined;
+  private _userEditFgSubscribed: Subscription | undefined;
+  private _subscribedRecaptcha: Subscription | undefined;
+
+  get KnownAsCtrl(): AbstractControl {
+    return this.userEditFg.get('knownAsCtrl') as FormControl;
+  }
+
+  get DateOfBirthCtrl(): FormControl {
+    return this.userEditFg.get('dateOfBirthCtrl') as FormControl;
+  }
+
+  get IntroductionCtrl(): AbstractControl {
+    return this.userEditFg.get('introductionCtrl') as FormControl;
+  }
+
+  get LookingForCtrl(): AbstractControl {
+    return this.userEditFg.get('lookingForCtrl') as FormControl;
+  }
+
+  get InterestsCtrl(): AbstractControl {
+    return this.userEditFg.get('interestsCtrl') as FormControl;
+  }
 
   ngOnInit(): void {
     // set datePicker year limitations
@@ -90,38 +122,12 @@ export class UserEditComponent implements OnInit, OnDestroy {
         if (member) {
           this.member = member;
 
-          this.initContollersValues(member);
+          this.initControllersValues(member);
         }
       });
   }
 
-  userEditFg: FormGroup = this._fb.group({
-    knownAsCtrl: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
-    dateOfBirthCtrl: [null, [Validators.required]],
-    introductionCtrl: ['', [Validators.maxLength(this.maxTextAreaChars)]],
-    lookingForCtrl: ['', [Validators.maxLength(this.maxTextAreaChars)]],
-    interestsCtrl: ['', [Validators.maxLength(this.maxTextAreaChars)]]
-  });
-
-  recaptchaCtrl: FormControl = this._fb.control({ value: false, disabled: true }, [Validators.required]);
-
-  get KnownAsCtrl(): AbstractControl {
-    return this.userEditFg.get('knownAsCtrl') as FormControl;
-  }
-  get DateOfBirthCtrl(): FormControl {
-    return this.userEditFg.get('dateOfBirthCtrl') as FormControl;
-  }
-  get IntroductionCtrl(): AbstractControl {
-    return this.userEditFg.get('introductionCtrl') as FormControl;
-  }
-  get LookingForCtrl(): AbstractControl {
-    return this.userEditFg.get('lookingForCtrl') as FormControl;
-  }
-  get InterestsCtrl(): AbstractControl {
-    return this.userEditFg.get('interestsCtrl') as FormControl;
-  }
-
-  initContollersValues(member: Member) {
+  initControllersValues(member: Member) {
     this.KnownAsCtrl.setValue(member.knownAs);
     this.DateOfBirthCtrl.setValue(member.dateOfBirth);
     this.IntroductionCtrl.setValue(member.introduction);
@@ -161,17 +167,12 @@ export class UserEditComponent implements OnInit, OnDestroy {
       && this.member.state === this._googlePlacesService.stateSig()
       && this.member.city === this._googlePlacesService.citySig()
     ) {
-
       this._commonService.isPreventingLeavingPage = false;
-
-      this.recaptchaCtrl.disable();
 
       return false;
     }
 
     this._commonService.isPreventingLeavingPage = true;
-
-    this.recaptchaCtrl.enable();
 
     return true;
   }
@@ -202,7 +203,11 @@ export class UserEditComponent implements OnInit, OnDestroy {
         .subscribe({
           next: (response: ApiResponseMessage) => {
             if (response.message) {
-              this._matSnak.open(response.message, "Close", { horizontalPosition: 'center', verticalPosition: 'bottom', duration: 10000 });
+              this._matSnak.open(response.message, "Close", {
+                horizontalPosition: 'center',
+                verticalPosition: 'bottom',
+                duration: 10000
+              });
 
               const loggedInUser: LoggedInUser | null = this.getLoggedInUserLocalStorage();
 
