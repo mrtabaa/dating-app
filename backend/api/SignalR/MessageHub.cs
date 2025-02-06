b176f4f6103149f297a9d437aca8f3da0680cc20
Reza Taba <mrtabaa@gmail.com>
2025-02-06 01:11:22 +0100

    Improved Unauthorized by sending error message to the client

--- backend/api/SignalR/MessageHub.cs
+++ backend/api/SignalR/MessageHub.cs
@@ -13,18 +13,26 @@ public class MessageHub(
         if (string.IsNullOrEmpty(targetUserName))
             throw new HubException("targetUserName cannot be null or empty.");
 
-        ObjectId userId = await GetUserId();
+        ObjectId? userId = await GetUserId();
+        if (userId is null)
+        {
+            await Clients.Caller.SendAsync(SignalRMessages.SendingError, "Unauthorized.", GetCancellationToken());
+            return;
+        }
 
-        string groupName = GetGroupName(await GetUserName(), targetUserName);
+        string groupName = GetGroupName(
+            await GetUserName() ?? throw new ArgumentNullException(nameof(GetGroupName))
+            , targetUserName
+        );
 
-        await AddGroupNameToDb(userId, groupName);
+        await AddGroupNameToDb(userId.Value, groupName);
 
         // TODO: Handle exceptions with middleware
         try
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-            await UpdateReadOnAsync(userId, targetUserName, groupName, MessageOperation.Join);
+            await UpdateReadOnAsync(userId.Value, targetUserName, groupName, MessageOperation.Join);
         }
         catch (Exception ex)
         {
@@ -37,8 +45,9 @@ public class MessageHub(
         ObjectId userId, string targetUserName, string groupName, MessageOperation operation
     )
     {
-        OperationResult<ObjectId> receiverUserIdResult =
-            await userRepository.GetIdByUserNameAsync(targetUserName.ToUpper(), GetCancellationToken());
+        OperationResult<ObjectId> receiverUserIdResult = await userRepository.GetIdByUserNameAsync(
+            targetUserName.ToUpper(), GetCancellationToken()
+        );
         if (!receiverUserIdResult.IsSuccess)
             throw new HubException("OtherUserId is invalid.");
 
@@ -87,44 +96,73 @@ public class MessageHub(
         {
             messageInDto.Content = messageInDto.Content.Trim();
 
-        if (ValidateContent(messageInDto.Content))
-            return;
+            if (ValidateContent(messageInDto.Content))
+                return;
 
-        ObjectId userId = await GetUserId();
+            ObjectId? userId = await GetUserId();
 
-        MessageDto messageDto = await messageRepository.CreateAsync(userId, messageInDto, GetCancellationToken())
-                                ?? throw new HubException("MessageDto is null. Message creation failed. Try again.");
+            if (userId is null)
+            {
+                await Clients.Caller.SendAsync(SignalRMessages.SendingError, "Unauthorized.", GetCancellationToken());
+                return;
+            }
 
-        _ = messageDto.UserOrTargetUserName
-            ?? throw new HubException("UserOrTargetUserName is null. Message creation failed. Try again.");
+            MessageDto messageDto = await messageRepository.CreateAsync(
+                                        userId.Value, messageInDto, GetCancellationToken()
+                                    )
+                                    ?? throw new HubException(
+                                        "MessageDto is null. Message creation failed. Try again."
+                                    );
 
-        string groupName = GetGroupName(await GetUserName(), messageInDto.ReceiverUserName);
+            _ = messageDto.UserOrTargetUserName
+                ?? throw new HubException("UserOrTargetUserName is null. Message creation failed. Try again.");
 
-        OperationResult<ObjectId> receiverUserIdResult =
-            await userRepository.GetIdByUserNameAsync(messageInDto.ReceiverUserName, GetCancellationToken());
-        if (!receiverUserIdResult.IsSuccess)
-            throw new HubException("OtherUserId is invalid.");
-
-        // Update and get ReadOn if target member is in a group. Also update messageDto.ReadOn 
-        if (await messageService.CheckIsMemberIsInGroupAsync(
-                receiverUserIdResult.Result, groupName, GetCancellationToken()
-            ))
-            messageDto.ReadOn = await UpdateReadOnAsync(
-                userId, messageInDto.ReceiverUserName, groupName, MessageOperation.Create
+            string groupName = GetGroupName(
+                await GetUserName() ?? throw new ArgumentNullException(nameof(GetGroupName))
+                , messageInDto.ReceiverUserName
             );
 
-        await Clients.Group(groupName).SendAsync(SignalRMessages.NewMessageRes, messageDto, GetCancellationToken());
+            OperationResult<ObjectId> receiverUserIdResult = await userRepository.GetIdByUserNameAsync(
+                messageInDto.ReceiverUserName, GetCancellationToken()
+            );
+            if (!receiverUserIdResult.IsSuccess)
+                throw new HubException("OtherUserId is invalid.");
+
+            // Update and get ReadOn if target member is in a group. Also update messageDto.ReadOn 
+            if (await messageService.CheckIsMemberIsInGroupAsync(
+                    receiverUserIdResult.Result, groupName, GetCancellationToken()
+                ))
+            {
+                messageDto.ReadOn = await UpdateReadOnAsync(
+                    userId.Value, messageInDto.ReceiverUserName, groupName, MessageOperation.Create
+                );
+            }
+
+            await Clients.Group(groupName).SendAsync(SignalRMessages.NewMessageRes, messageDto, GetCancellationToken());
+        }
+        finally
+        {
+            semaphore.Release();
+        }
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        ObjectId userId = await GetUserId();
+        ObjectId? userId = await GetUserId();
+        if (userId is null)
+        {
+            await Clients.Caller.SendAsync(SignalRMessages.SendingError, "Unauthorized.", GetCancellationToken());
+            return;
+        }
 
-        MessageGroup messageGroup =
-            await messageService.GetMessageGroupAsync(userId, Context.ConnectionId, GetCancellationToken())
-            ?? throw new HubException("MessageGroup is not found. DB MessageGroup removal will fail.");
+        MessageGroup messageGroup = await messageService.GetMessageGroupAsync(
+                                        userId.Value, Context.ConnectionId, GetCancellationToken()
+                                    )
+                                    ?? throw new HubException(
+                                        "MessageGroup is not found. DB MessageGroup removal will fail."
+                                    );
 
-        if (!await messageService.RemoveMessageGroupAsync(userId, messageGroup))
+        if (!await messageService.RemoveMessageGroupAsync(userId.Value, messageGroup))
             throw new HubException("Removing MessageGroup from DB failed.");
 
         await base.OnDisconnectedAsync(exception);
@@ -141,18 +179,23 @@ public class MessageHub(
             throw new HubException("Add messageGroup to DB failed.");
     }
 
-    private async Task<string> GetUserName()
+    private async Task<string?> GetUserName()
     {
-        string userIdHashed = Context.User?.GetUserIdHashed()
-                              ?? throw new HubException("The token is invalid/expired. Login again.");
+        string? userIdHashed = Context.User?.GetUserIdHashed();
+        if (string.IsNullOrEmpty(userIdHashed))
+        {
+            await Clients.Caller.SendAsync(SignalRMessages.SendingError, "Unauthorized.", GetCancellationToken());
+            return null;
+        }
 
         return await userRepository.GetUserNameByIdentifierHashAsync(userIdHashed, GetCancellationToken())
                ?? throw new HubException("UserName is invalid. Login again.");
     }
 
-    private async Task<ObjectId> GetUserId() =>
-        await tokenService.GetActualUserIdAsync(Context.User?.GetUserIdHashed(), GetCancellationToken())
-        ?? throw new HubException("UserId is invalid. Login again.");
+    private async Task<ObjectId?> GetUserId() =>
+        await tokenService.GetActualUserIdAsync(
+            Context.User?.GetUserIdHashed(), GetCancellationToken()
+        );
 
     private static bool ValidateContent(string content)
     {
