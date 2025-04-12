d43ef611574330d045b5b9e572e3e776eb927c3a
Reza Taba <mrtabaa@gmail.com>
2025-04-12 16:30:43 -0400

    Improved message

--- client/src/app/interceptors/jwt.interceptor.ts
+++ client/src/app/interceptors/jwt.interceptor.ts
@@ -1,8 +1,8 @@
-import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
-import {AccountService} from "../services/account.service";
-import {inject} from "@angular/core";
-import {catchError, switchMap, throwError} from 'rxjs';
-import {MatSnackBar} from "@angular/material/snack-bar";
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { AccountService } from "../services/account.service";
+import { inject } from "@angular/core";
+import { catchError, switchMap, throwError } from 'rxjs';
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 let refreshTokenInProgress = false;
 
@@ -32,6 +32,13 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
             console.error(error);
             refreshTokenInProgress = false; // Reset flag if refresh fails
             accountService.logout();
+
+            snack.open('Your session has expired. Please login again.', 'Close', {
+              horizontalPosition: 'center',
+              verticalPosition: 'top',
+              duration: 7000
+            });
+
             return throwError(() => error);
           })
         );
