d006e0888def2e1dfbdbdf216db39dd711286789
Reza Taba <mrtabaa@gmail.com>
2025-04-30 15:17:36 +0200

    Converted absolute URLs to relative

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://da-api-mr.azurewebsites.net/api/',
-  apiPhotoUrl: 'https://da-api-mr.azurewebsites.net/',
-  hubUrl: 'https://da-api-mr.azurewebsites.net/hubs/' // added hubUrl
+  apiUrl: '//da-api-mr.azurewebsites.net/api/',
+  apiPhotoUrl: '//da-api-mr.azurewebsites.net/',
+  hubUrl: '//da-api-mr.azurewebsites.net/hubs/' // added hubUrl
 };
