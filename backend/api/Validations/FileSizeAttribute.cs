aaa79273d0c98c5f0dd25a7baae4d1d77b3f1b57
Reza Taba <mrtabaa@gmail.com>
2026-07-08 18:58:05 +0300

    feat(api): add file size check for multiple files as well as one.

--- backend/api/Validations/FileSizeAttribute.cs
+++ backend/api/Validations/FileSizeAttribute.cs
@@ -1,26 +1,70 @@
-namespace api.Extensions.Validations;
+using System.ComponentModel.DataAnnotations;
 
-/// <summary>
-/// Set a minimum and maximum size for input files.
-/// </summary>
-/// <param name="minFileSize"></param>
-/// <param name="maxFileSize"></param>
-public class FileSizeAttribute(long minFileSize, long maxFileSize) : ValidationAttribute
+namespace api.Validations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false)]
+public sealed class FileSizeAttribute(long minFileSize, long maxFileSize) : ValidationAttribute
 {
-    protected override ValidationResult? IsValid(object? values, ValidationContext validationContext)
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        var file = values as IFormFile;
+        if (minFileSize < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(minFileSize), "Minimum file size cannot be negative.");
+        }
 
-        if (file is not null)
-            if (file is not null && file.Length < minFileSize)
+        if (maxFileSize < minFileSize)
+        {
+            throw new ArgumentOutOfRangeException(nameof(maxFileSize), "Maximum file size cannot be less than minimum file size.");
+        }
+
+        IEnumerable<IFormFile>? files = value switch
+        {
+            IFormFile singleFile => [singleFile],
+            IEnumerable<IFormFile> multipleFiles => multipleFiles,
+            _ => null
+        };
+
+        if (files is null)
+        {
+            return ValidationResult.Success;
+        }
+
+        foreach (var file in files)
+        {
+            if (file is null)
             {
-                return new ValidationResult($"One or more of the files is below the allowed minimum size of {minFileSize / 1_000} kilobytes.");
+                continue;
             }
-            else if (file is not null && file.Length > maxFileSize)
+
+            var fileName = Path.GetFileName(file.FileName);
+
+            if (file.Length < minFileSize)
             {
-                return new ValidationResult($"One or more of the files is over the allowed maximum size of {maxFileSize / 1_000_000} megabytes.");
+                return new ValidationResult(
+                    $"File '{fileName}' is {ToKb(file.Length)} KB, which is below the allowed minimum size of {ToKb(minFileSize)} KB.",
+                    GetMemberNames(validationContext));
             }
+
+            if (file.Length > maxFileSize)
+            {
+                return new ValidationResult(
+                    $"File '{fileName}' is {ToKb(file.Length)} KB, which exceeds the allowed maximum size of {ToKb(maxFileSize)} KB.",
+                    GetMemberNames(validationContext));
+            }
+        }
 
         return ValidationResult.Success;
+    }
+
+    private static string ToKb(long bytes)
+    {
+        return (bytes / 1024.0).ToString("F2");
+    }
+
+    private static IEnumerable<string> GetMemberNames(ValidationContext context)
+    {
+        return string.IsNullOrWhiteSpace(context.MemberName)
+            ? []
+            : [context.MemberName];
     }
 }
